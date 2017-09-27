f74c10cf4f8c644175925c0a9cf7b85ae09c616f
Dr Kim Foale <kim@alliscalm.net>
2017-09-27 15:05:26 +0100

    Move Google API key to environment variable

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,11 +1,11 @@
 Geocoder.configure(
-  :google_places_search => {
-    :api_key => 'AIzaSyAVpTXMhQcfXrUGt1eT5nW0FHvUCW-x_AE'
+  google_places_search: {
+    api_key: ENV['GOOGLE_API_KEY']
   },
-  :google_places_details => {
-    :api_key => 'AIzaSyAVpTXMhQcfXrUGt1eT5nW0FHvUCW-x_AE'
+  google_places_details: {
+    api_key: ENV['GOOGLE_API_KEY']
   },
-  :google => {
-    :api_key => 'AIzaSyAVpTXMhQcfXrUGt1eT5nW0FHvUCW-x_AE'
+  google: {
+    api_key: ENV['GOOGLE_API_KEY']
   }
 )
