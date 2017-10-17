58736d92b01d51a66bdf4c0f7bb6c4ca46618462
Dr Kim Foale <kim@alliscalm.net>
2017-10-17 17:51:25 +0100

    Resonsive temp grids

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
   def markdown(source)
     if source
-      auto_link Kramdown::Document.new(source).to_html.html_safe
+      auto_link Kramdown::Document.new(source).to_html.html_safe do |text|
+        truncate(text, length: 30)
+      end
     else
       ''
     end
