19b6e79d257f17d9a153279b1ab0709c10aee8a9
Dr Kim Foale <kim@alliscalm.net>
2017-11-10 12:57:29 +0000

    Unstyled iframe comlete

--- public/widget.js
+++ public/widget.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  
   // Load params
   var widget = document.getElementById('placecal_widget');
   var place = widget.getAttribute('data-place');
@@ -7,13 +8,13 @@ window.onload = function() {
   var height = widget.getAttribute('data-height') || 700;
 
   // Load iFrame
-  var iframe = document.createElement('iframe');
+  var iframe = document.getElementById('placecal_results');
   iframe.style.border = 'none';
   iframe.style.margin = 0;
   iframe.style.padding = 0;
   iframe.style.width = width;
   iframe.style.height = height;
   iframe.src = 'http://localhost:3000/places/' + place + '/embed';
-  console.log(iframe);
   document.body.appendChild(iframe);
+
 };
