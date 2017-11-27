847a431a726dd670bfb4a96acd363f0a61f18585
Dr Kim Foale <kim@alliscalm.net>
2017-11-27 16:15:22 +0000

    Fix tests

--- test/factories/partner.rb
+++ test/factories/partner.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name "Hulme Community Garden Centre"
     admin_email nil
     admin_name nil
-    logo nil
+    image nil
     public_email nil
     public_phone nil
     short_description nil
