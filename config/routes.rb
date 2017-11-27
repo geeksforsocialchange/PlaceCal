f596cbb7870731fbc1f00c4b59af48b08b493dc5
Dr Kim Foale <kim@alliscalm.net>
2017-11-27 15:22:21 +0000

    Fix partner time pagination

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,31 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   # Most common route at the top
   root 'pages#home'
 
-  # Core resources
-  resources :events, only: %i[index show]
-  get '/events/:year/:month/:day' => 'events#index', constraints: {
+  ymd = {
     year:       /\d{4}/,
     month:      /\d{1,2}/,
     day:        /\d{1,2}/
   }
+
+  # Events
+  resources :events, only: %i[index show]
+  get '/events/:year/:month/:day' => 'events#index', constraints: ymd
+
+  # Places
   resources :places, only: %i[index show]
   get '/places/:id/events' => 'places#show'
-  get '/places/:id/events/:year/:month/:day' => 'places#show', constraints: {
-    year:       /\d{4}/,
-    month:      /\d{1,2}/,
-    day:        /\d{1,2}/
-  }
+  get '/places/:id/events/:year/:month/:day' => 'places#show', constraints: ymd
   get '/places/:id/embed' => 'places#embed'
+
+  # Partners
   resources :partners, only: %i[index show]
+  get '/partners/:id/events' => 'partners#show'
+  get '/partners/:id/events/:year/:month/:day' => 'partners#show', constraints: ymd
+
+  # Calendars
   resources :calendars, only: %i[index show]
+
+  # Collections
   resources :collections, only: %i[show]
 
   # Users
