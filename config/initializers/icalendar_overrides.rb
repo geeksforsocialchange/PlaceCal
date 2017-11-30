9826af33755958ab6a5481693c05f8c89271d6c5
GabAlexandria <gabagrant@gmail.com>
2017-11-30 14:46:44 -0400

    fix timezone bug

--- config/initializers/icalendar_overrides.rb
+++ config/initializers/icalendar_overrides.rb
@@ -1,5 +1,7 @@
 Icalendar::Recurrence::Schedule.class_eval do
   def timezone
+    return Time.zone.name if event.tzid.blank?
+
     ActiveSupport::TimeZone[event.tzid].present? ? event.tzid : Time.zone.name
   end
 end
