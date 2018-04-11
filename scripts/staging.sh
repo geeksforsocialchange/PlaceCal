1520a9cbf58c9ec63675121ef6b6b26bcc3fa10d
GabAlexandria <gabagrant@gmail.com>
2018-04-11 12:29:56 -0400

    another attempt to get it to recognize the decrypted key

--- scripts/staging.sh
+++ scripts/staging.sh
@@ -1,6 +1,6 @@
 eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 deploy.key # this key should have push access
-ssh-add deploy.key
+chmod 600 ./deploy.key # this key should have push access
+ssh-add ./deploy.key
 ssh-keyscan placecal-staging.org >> ~/.ssh/known_hosts
 git remote add deploy dokku@placecal-staging.org:placecal-staging
 git config --global push.default simple
