c994e4a92550acea0395fc5ddb7ce51310aaeef3
GabAlexandria <gabagrant@gmail.com>
2018-04-12 09:57:49 -0400

    more CI deploy debugging

--- scripts/staging.sh
+++ scripts/staging.sh
@@ -1,6 +1,8 @@
 eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 ./deploy.key # this key should have push access
-ssh-add ./deploy.key
+echo $PWD
+echo ls -a
+chmod 600 $TRAVIS_BUILD_DIR/deploy.key # this key should have push access
+ssh-add $TRAVIS_BUILD_DIR/deploy.key
 ssh-keyscan placecal-staging.org >> ~/.ssh/known_hosts
 git remote add deploy dokku@placecal-staging.org:placecal-staging
 git config --global push.default simple
