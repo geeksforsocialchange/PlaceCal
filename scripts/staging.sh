626f600e7f3159c1bc103413595dcb1763e93bd8
GabAlexandria <gabagrant@gmail.com>
2018-04-12 10:27:33 -0400

    skip artifacts clean up before deploy

--- scripts/staging.sh
+++ scripts/staging.sh
@@ -1,8 +1,6 @@
 eval "$(ssh-agent -s)" #start the ssh agent
-echo $PWD
-ls -a
-chmod 600 $TRAVIS_BUILD_DIR/deploy.key # this key should have push access
-ssh-add $TRAVIS_BUILD_DIR/deploy.key
+chmod 600 deploy.key # this key should have push access
+ssh-add deploy.key
 ssh-keyscan placecal-staging.org >> ~/.ssh/known_hosts
 git remote add deploy dokku@placecal-staging.org:placecal-staging
 git config --global push.default simple
