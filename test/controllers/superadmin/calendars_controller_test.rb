eb1518c50d3801198852b4fb1f03a46ff4cd0c6b
Dr Kim Foale <kim@gfsc.studio>
2018-05-08 13:21:48 +0100

    One superadmin test for completeness

--- test/controllers/superadmin/calendars_controller_test.rb
+++ test/controllers/superadmin/calendars_controller_test.rb
@@ -4,18 +4,39 @@ class SuperadminCalendarsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @calendar = create(:calendar)
     @root = create(:root)
+    @citizen = create(:user)
+
+    host! 'lvh.me'
   end
 
   it_allows_access_to_index_for(%i[root]) do
     get superadmin_calendars_url
+    assert_response :success
+  end
+
+  it_denies_access_to_index_for(%i[citizen]) do
+    get superadmin_calendars_url
+    assert_redirected_to root_url
   end
 
   it_allows_access_to_show_for(%i[root]) do
     get superadmin_calendar_url(@calendar)
+    assert_response :success
+  end
+
+  it_denies_access_to_show_for(%i[citizen]) do
+    get superadmin_calendar_url(@calendar)
+    assert_redirected_to root_url
   end
 
   it_allows_access_to_new_for(%i[root]) do
     get new_superadmin_calendar_url
+    assert_response :success
+  end
+
+  it_denies_access_to_new_for(%i[citizen]) do
+    get new_superadmin_calendar_url
+    assert_redirected_to root_url
   end
 
   it_allows_access_to_create_for(%i[root]) do
@@ -28,11 +49,21 @@ class SuperadminCalendarsControllerTest < ActionDispatch::IntegrationTest
   it_allows_access_to_update_for(%i[root]) do
     patch superadmin_calendar_url(@calendar),
       params: { calendar: { name: 'New Test Calendar Name' } }
+    assert_redirected_to superadmin_calendar_url(@calendar)
   end
 
   it_allows_access_to_destroy_for(%i[root]) do
     assert_difference('Calendar.count', -1) do
       delete superadmin_calendar_url(@calendar)
     end
+  end
+
+  test "redirects if not logged in" do
+    get superadmin_calendars_url
+    assert_redirected_to new_user_session_path
+    get superadmin_calendar_url(@calendar)
+    assert_redirected_to new_user_session_path
+    get new_superadmin_calendar_url
+    assert_redirected_to new_user_session_path
   end
 end
