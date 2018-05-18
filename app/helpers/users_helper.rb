ca9a9a5aa8fee3f65b8fe9e34ee0da687c545752
Dr Kim Foale <kim@gfsc.studio>
2018-05-18 16:09:33 +0100

    Add gravatars for now

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
+# app/helpers/users_helper.rb
 module UsersHelper
+  def gravatar_for(user, size: 200)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.full_name, class: 'gravatar')
+  end
 end
