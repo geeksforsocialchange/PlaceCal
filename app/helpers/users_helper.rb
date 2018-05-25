13581002f778b9f9530e43380f8fbe4138ebe66c
Dr Kim Foale <kim@gfsc.studio>
2018-05-25 13:46:23 +0100

    Portrait layout

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@
 
 # app/helpers/users_helper.rb
 module UsersHelper
-  def gravatar_for(user, size: 200)
+  def gravatar_for(user, size: 240)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url,
