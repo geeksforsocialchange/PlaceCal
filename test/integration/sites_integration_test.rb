5833a243fe724a10e8087bcf8203d5e448f47e73
Dr Kim Foale <kim@gfsc.studio>
2018-05-28 15:40:55 +0100

    Site sanity tests

--- test/integration/sites_integration_test.rb
+++ test/integration/sites_integration_test.rb
@@ -4,7 +4,8 @@ require 'test_helper'
 
 class SitesIntegrationTest < ActionDispatch::IntegrationTest
   setup do
-    @site = create(:site, slug: 'hulme')
+    @admin = create(:user)
+    @site = create(:site, slug: 'hulme', site_admin: @admin)
   end
 
   test 'load different pages based on subdomain' do
@@ -17,5 +18,19 @@ class SitesIntegrationTest < ActionDispatch::IntegrationTest
     # If there's a match, display a custom homepage
     get 'http://hulme.lvh.me'
     assert_template 'sites/default.html.erb'
+  end
+
+  test 'basic page content shows up' do
+    get 'http://hulme.lvh.me'
+    assert_select 'h1', 'PlaceCal is a community events calendar where you can find everything near you, all in one place.'
+    assert_select 'h2', "PlaceCal is working to make #{@site.name} a better connected neighbourhood."
+    assert_select 'p', @site.description
+    assert_select 'strong', @admin.full_name
+    assert_select 'strong', @admin.phone
+    assert_select 'strong', @admin.email
+    assert_select 'h3', 'Adding Your Events'
+    assert_select 'h3', 'Getting Online'
+    assert_select 'h3', 'PlaceCal Support'
+    assert_select 'p', 'Information about getting online coming soon.'
   end
 end
