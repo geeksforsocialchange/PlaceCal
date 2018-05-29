51ebf334ee7652994ed2e4382bd8f9acf3df71c9
GabAlexandria <gabagrant@gmail.com>
2018-05-29 07:01:13 -0400

    fix sites turfs duplication, allow secondary turfs to be removed

--- app/models/sites_turf.rb
+++ app/models/sites_turf.rb
@@ -5,5 +5,5 @@ class SitesTurf < ApplicationRecord
   self.table_name = 'sites_turfs'
   belongs_to :turf
   belongs_to :site
-  validates_uniqueness_of :turf_id, scope: :site_id
+  validates_uniqueness_of :turf_id, scope: :site_id, message: 'Turf can not be assigned more than once to a site'
 end
