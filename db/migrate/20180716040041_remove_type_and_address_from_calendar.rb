00fd85805b22534f44e216c14e842bc46f589201
GabAlexandria <gabagrant@gmail.com>
2018-07-18 09:51:12 -0400

    remove import lock at column

--- db/migrate/20180716040041_remove_type_and_address_from_calendar.rb
+++ db/migrate/20180716040041_remove_type_and_address_from_calendar.rb
@@ -2,5 +2,6 @@ class RemoveTypeAndAddressFromCalendar < ActiveRecord::Migration[5.1]
   def change
     remove_column :calendars, :type, :string
     remove_column :calendars, :address_id, :integer
+    remove_column :calendars, :import_lock_at, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20180716040041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "strategy"
-    t.datetime "import_lock_at"
     t.string "last_checksum"
     t.text "footer"
     t.index ["partner_id"], name: "index_calendars_on_partner_id"
