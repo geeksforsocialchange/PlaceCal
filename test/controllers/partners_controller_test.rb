fe3e5a009b4ad375c39b107e7367efef793969b0
Justin Hellings <justin.hellings@gmail.com>
2018-08-16 15:22:01 +0100

    Added tests for subsite partner/place filtering.

--- test/controllers/partners_controller_test.rb
+++ test/controllers/partners_controller_test.rb
@@ -4,16 +4,32 @@ require 'test_helper'
 
 class PartnersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @partner = create(:partner)
+    @partners = [ create(:partner), create(:partner) ]
+    @site = build(:site)
+    @site.turfs.append(@partners.first.turfs.first)
+    @site.save
   end
 
-  test 'should get index' do
-    get partners_url
+  test 'should get index without subdomain' do
+    get url_for controller: "partners", subdomain: false
     assert_response :success
+    assert_select "ul.partners li", 2
+  end
+
+  test 'should get index with configured subdomain' do
+    get url_for controller: "partners", subdomain: @site.slug
+    assert_response :success
+    assert_select "ul.partners li", 1
+  end
+
+  test 'should get index with unknown subdomain' do
+    get url_for controller: "partners", subdomain: "notaknownsubdomain"
+    assert_response :success
+    assert_select "ul.partners li", 2
   end
 
   test 'should show partner' do
-    get partner_url(@partner)
+    get partner_url(@partners.first)
     assert_response :success
   end
 end
--- test/controllers/places_controller_test.rb
+++ test/controllers/places_controller_test.rb
@@ -4,16 +4,32 @@ require 'test_helper'
 
 class PlacesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @place = create(:place)
+    @places = [ create(:place), create(:place) ]
+    @site = build(:site)
+    @site.turfs.append(@places.first.turfs.first)
+    @site.save
   end
 
-  test 'should get index' do
-    get places_url
+  test 'should get index without subdomain' do
+    get url_for controller: "places", subdomain: false
     assert_response :success
+    assert_select "ul.places li", 2
+  end
+
+  test 'should get index with configured subdomain' do
+    get url_for controller: "places", subdomain: @site.slug
+    assert_response :success
+    assert_select "ul.places li", 1
+  end
+
+  test 'should get index with unknown subdomain' do
+    get url_for controller: "places", subdomain: "notaknownsubdomain"
+    assert_response :success
+    assert_select "ul.places li", 2
   end
 
   test 'should show place' do
-    get place_url(@place)
+    get place_url(@places.first)
     assert_response :success
   end
 end
