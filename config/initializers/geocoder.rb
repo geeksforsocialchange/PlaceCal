cbe46efa2890bdb2937f9bad1fb7843f8b7a7448
Justin Hellings <justin.hellings@gmail.com>
2018-09-06 12:59:56 +0100

    Commented use of Geocoder that could no longer work

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -5,14 +5,4 @@ Geocoder.configure(
   lookup: :postcodes_io,
 
   timeout: 5
-  
-  # google_places_search: {
-  #   api_key: ENV['GOOGLE_API_KEY']
-  # },
-  # google_places_details: {
-  #   api_key: ENV['GOOGLE_API_KEY']
-  # },
-  # google: {
-  #   api_key: ENV['GOOGLE_API_KEY']
-  # }
 )
