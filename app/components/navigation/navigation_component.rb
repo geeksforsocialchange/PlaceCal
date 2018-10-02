d8223cdec9bb3645ff6175d7f1bf8062dbc61256
Justin Hellings <justin.hellings@gmail.com>
2018-10-02 16:59:18 +0100

    Added file. should have been in prev commit.

---
+++ app/components/navigation/navigation_component.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# app/components/navigation/navigation_component.rb
+class NavigationComponent < MountainView::Presenter
+
+  property :navigation
+
+  # property :site
+  #
+  # def slug
+  #   site&.slug
+  # end
+  #
+  # def site_logo
+  #   site.logo
+  # end
+
+end
