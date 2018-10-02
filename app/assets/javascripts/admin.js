b34a70a6007430b0869f525f24648f5cf00b266c
GabAlexandria <gabagrant@gmail.com>
2018-10-02 10:52:35 -0400

    add address fields to partners, general clean up

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,7 +2,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require jquery_nested_form
+//= require cocoon
 //= require select2
 //= require leaflet
 //= require_tree ./behaviors
