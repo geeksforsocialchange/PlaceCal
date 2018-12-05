7c3641567d05e5063b1dcda62592a0b2bba0acb6
Justin Hellings <justin.hellings@gmail.com>
2018-12-05 14:42:17 +0000

    Tested partner-identification for event location.

--- test/system/events_import_test.rb
+++ test/system/events_import_test.rb
@@ -11,33 +11,23 @@ class EventsImportTest < ActiveSupport::TestCase
       source: 'https://calendar.google.com/calendar/ical/mgemn0rmm44un8ucifb287coto%40group.calendar.google.com/public/basic.ics'
     )
 
+    partner = create(:partner, name: 'Z-ARTS')
+
     VCR.use_cassette(:import_test_calendar) do
       calendar.import_events(Date.new(2018,11,20))
     end
 
-    # pp Event.all.map {|e| [e.address_id, e.raw_location_from_source] }
-
-    # The Old Abbey Taphouse, Guildhall Cl, Manchester M15 6SY
-    # Old Abbey Taphouse, Guildhall Cl, Manchester M15 6SY
-    # The Old Abbey Taphouse, Guildhall Cl, Manchester M15 6SY, UK
-
-    " Old Abbey Taphouse, Guildhall Cl, Manchester, M15 6SY"
-    # Old Abbey Taphouse, Guildhall Cl, Manchester, M15 6SY
-    " Old Abbey Taphouse Guildhall Cl Manchester M15 6SY"
-    # Old Abbey Taphouse Guildhall Cl Manchester M15 6SY
-    " oLd AbBeY tApHoUsE, Guildhall Cl, Manchester, M15 6SY"
-    # oLd AbBeY tApHoUsE, Guildhall Cl, Manchester, M15 6SY
-    " Guildhall Cl, Manchester M15 6SY"
-    # Guildhall Cl, Manchester M15 6SY
-    " Old Abbey Taphouse, Guildhall Cl, Manchester M15 6SY UK"
-    # Old Abbey Taphouse, Guildhall Cl, Manchester M15 6SY UK
-
-
     # pp Event.all
     # pp Address.all
 
+    # Did all events import?
     assert_equal 10, Event.all.count
+
+    # Was only one address created by the import? (One address was created for partner.)
     assert_equal 2, Address.all.count
+
+    # Did exactly two events get identified as being held at partner?
+    assert_equal 2, Event.where( place: partner ).count
 
   end
 
