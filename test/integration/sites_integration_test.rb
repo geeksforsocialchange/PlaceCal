86802080ab06c47fa949fe0cf27bdc0bd61d3457
Dr Kim Foale <kim@gfsc.studio>
2019-06-05 14:37:53 +0100

    - Add Mossley OG image - Add place_name field to sites - Improve meta tags

--- test/integration/sites_integration_test.rb
+++ test/integration/sites_integration_test.rb
@@ -25,7 +25,7 @@ class SitesIntegrationTest < ActionDispatch::IntegrationTest
   test 'basic page content shows up' do
     get 'http://hulme.lvh.me'
     assert_select 'h1', 'PlaceCal is a community events calendar where you can find everything near you, all in one place.'
-    assert_select 'h2', "PlaceCal is working to make #{@site.name} a better connected neighbourhood."
+    assert_select 'h2', "PlaceCal is working to make #{@site.place_name} a better connected neighbourhood."
     assert_select 'p', @site.description
     assert_select 'strong', @admin.full_name
     assert_select 'strong', @admin.phone
