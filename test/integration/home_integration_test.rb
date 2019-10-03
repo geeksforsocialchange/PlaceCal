4e1d7744ce5b6bb3c49e9c0629ffbd3f5782cebe
Dr Kim Foale <kim@gfsc.studio>
2019-10-03 17:03:45 +0100

    Refactor page titles and add an ADR with the logic

--- test/integration/home_integration_test.rb
+++ test/integration/home_integration_test.rb
@@ -3,23 +3,19 @@
 require 'test_helper'
 
 class HomeIntegrationTest < ActionDispatch::IntegrationTest
-  setup do
-    # Create a default site and a neighbourhood one
-    @default_site = create_default_site
-    @neighbourhood_site = create(:site_local)
-  end
-
   test 'home page for default site has correct title' do
-    get root_url
+    @default_site = create_default_site
+    get "http://lvh.me"
     assert_response :success
     assert_select 'title', count: 1, text: "PlaceCal | The Community Calendar"
     assert_select 'h1', count: 1, text: 'Welcome to PlaceCal'
   end
 
   test 'home page for neighbourhood site has correct title' do
+    @neighbourhood_site = create(:site_local)
     get "http://#{@neighbourhood_site.slug}.lvh.me"
     assert_response :success
-    assert_select 'title', count: 1, text: "#{@neighbourhood_site.name} | The Community Calendar"
+    assert_select 'title', count: 1, text: "#{@neighbourhood_site.name}"
     assert_select 'h1', count: 1, text: 'PlaceCal is a community events calendar where you can find everything near you, all in one place.'
   end
 end
