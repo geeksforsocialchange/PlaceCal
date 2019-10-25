ce903c28ae3ab13d7a1f54fa41262235cf86d61a
Dr Kim Foale <kim@gfsc.studio>
2019-10-25 13:30:48 +0100

    Add pseudocode tests

---
+++ test/policies/partner_policy_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class PartnerPolicyTest < ActiveSupport::TestCase
+  # setup do
+  #   guest_user
+  #   partner_admin
+  #   different_partner_admin
+  #   root_admin
+  # end
+
+  # def test_show
+  #   Everyone can see it
+  # end
+  #
+  # def test_create
+  #   Root admins can create
+  #   Everyone else can't create
+  # end
+  #
+  # def test_update
+  #   Partner admin, root admin can update
+  #   Different partner admin, guest can't
+  # end
+  #
+  # def test_destroy
+  #   Root admin only can destroy
+  # end
+end
