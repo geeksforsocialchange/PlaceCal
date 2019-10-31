73613a53d10a46ef32299fa922125a59c46c6b76
Dr Kim Foale <kim@gfsc.studio>
2019-10-31 18:22:40 +0000

    Add extra static files grr

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,3 +4,6 @@
 
 // TODO: Clear this out
 //= link 2017/12/placecal_winter2017.pdf
+//= link 2017/12/placecal_winter2017_bw.pdf
+//= link 2017/12/placecal_winter2017_poster.pdf
+//= link 2017/12/placecal_winter2017_poster_bw.pdf
