15d6f7882a4dd776281dad365a96749a1c91bee3
GabAlexandria <gabagrant@gmail.com>
2019-11-24 13:50:59 -0400

    add vue to webpack, add new select time vue library

--- app/javascript/src/opening-hours.js
+++ app/javascript/src/opening-hours.js
@@ -23,40 +23,6 @@ var daysArray = [
   { value: 6, name: 'Sunday', "schema.org": "http://schema.org/Sunday" },
 ];
 
-var timesArray = [
-  { value: 0, name: '12 am (midnight)', '24hour': '00:00:00' },
-  { value: 1, name: '1:00 am', '24hour': '01:00:00' },
-  { value: 2, name: '2:00 am', '24hour': '02:00:00' },
-  { value: 3, name: '3:00 am', '24hour': '03:00:00' },
-  { value: 4, name: '4:00 am', '24hour': '04:00:00' },
-  { value: 5, name: '5:00 am', '24hour': '05:00:00' },
-  { value: 6, name: '6:00 am', '24hour': '06:00:00' },
-  { value: 7, name: '7:00 am', '24hour': '07:00:00' },
-  { value: 8, name: '8:00 am', '24hour': '08:00:00' },
-  { value: 9, name: '9:00 am', '24hour': '09:00:00' },
-  { value: 10, name: '10:00 am', '24hour': '10:00:00' },
-  { value: 11, name: '11:00 am', '24hour': '11:00:00' },
-  { value: 12, name: '12:00 pm (noon)', '24hour': '12:00:00' },
-  { value: 13, name: '1:00 pm', '24hour': '13:00:00' },
-  { value: 14, name: '2:00 pm', '24hour': '14:00:00' },
-  { value: 15, name: '3:00 pm', '24hour': '15:00:00' },
-  { value: 16, name: '4:00 pm', '24hour': '16:00:00' },
-  { value: 17, name: '5:00 pm', '24hour': '17:00:00' },
-  { value: 18, name: '6:00 pm', '24hour': '18:00:00' },
-  { value: 19, name: '7:00 pm', '24hour': '19:00:00' },
-  { value: 20, name: '8:00 pm', '24hour': '20:00:00' },
-  { value: 21, name: '9:00 pm', '24hour': '21:00:00' },
-  { value: 22, name: '10:00 pm', '24hour': '22:00:00' },
-  { value: 23, name: '11:00 pm', '24hour': '23:00:00' },
-  { value: 24, name: '12:00 am (midnight next day)', '24hour': '00:00:00' },
-  { value: 25, name: '1:00 am (next day)', '24hour': '01:00:00' },
-  { value: 26, name: '2:00 am (next day)', '24hour': '02:00:00' },
-  { value: 27, name: '3:00 am (next day)', '24hour': '03:00:00' },
-  { value: 28, name: '4:00 am (next day)', '24hour': '04:00:00' },
-  { value: 29, name: '5:00 am (next day)', '24hour': '05:00:00' },
-  { value: 30, name: '6:00 am (next day)', '24hour': '06:00:00' },
-];
-
 // Convert an added day to a schema.org OpeningHoursSpecification.
 function openingHoursSpecification(day) {
   return {
@@ -74,6 +40,7 @@ function openingHoursSpecification(day) {
 //      ?  include EcmaScript 6 functions.
 //
 function addedDay(spec) {
+  console.log(spec);
   return {
     dayName: daysArray.reduce( function(acc,val) {
       if (acc) {
@@ -83,25 +50,7 @@ function addedDay(spec) {
       } else {
         return false
       }
-    }, false),
-    openingTimeName: timesArray.reduce( function(acc,val) {
-      if (acc) {
-        return acc;
-      } else if (val['24hour'] == spec.opens){
-        return val.name;
-      } else {
-        return false
-      }
-    }, false),
-    closingTimeName: timesArray.reduce( function(acc,val) {
-      if (acc) {
-        return acc;
-      } else if (val['24hour'] == spec.closes){
-        return val.name;
-      } else {
-        return false
-      }
-    }, false),
+    }, false)
 
     // OpeningHoursSpecification:
     closes: spec.closes,
@@ -146,26 +95,20 @@ new Vue({
     addedDays: openingHoursFromServer.map( addedDay ),
     openingHoursSpecifications: openingHoursFromServer,
     selectedDay: 0,
-    selectedOpeningTime: 9,
-    selectedClosingTime: 17,
+    selectedOpeningTime:  {
+      hh: '09',
+      mm: '00',
+      a: 'AM'
+    },
+    selectedClosingTime: {
+      hh: '05',
+      mm: '00',
+      a: 'PM'
+    },
     days: daysArray,
-    openingTimes: timesArray,
-    closingTimes: timesArray,
   },
 
   computed: {
-
-    filteredOpeningTimes: function() {
-      return this.openingTimes.filter(function (el) {
-        return el.value < this.selectedClosingTime;
-      }, this);
-    },
-
-    filteredClosingTimes: function() {
-      return this.closingTimes.filter(function (el) {
-        return el.value > this.selectedOpeningTime;
-      }, this);
-    },
 
     selectedDayName: function() {
       return this.days[this.selectedDay].name;
