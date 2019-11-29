480b5c9524f56f2a2e9bf02cf7a1904ec37030ee
Dr Kim Foale <kim@gfsc.studio>
2019-11-29 15:33:14 +0000

    Start calendar contact info

--- test/factories/partner.rb
+++ test/factories/partner.rb
@@ -3,16 +3,23 @@
 FactoryBot.define do
   factory(:partner) do
     sequence(:name) do |n|
-      "Hulme Garden Center #{n}"
+      "Community Group #{n}"
     end
-    # admin_email nil
-    # admin_name nil
-    # image nil
-    public_email { 'partner@placecal.org' }
+
+    public_name { 'John Q Public' }
+    public_email { 'contact@communitygroup.org' }
     public_phone { '0161 0000000' }
+
+    partner_email { 'admin@communitygroup.org' }
+    partner_name { 'Addy Minny Strator' }
+    partner_phone { '0161 0000001' }
+
     short_description { 'A cool garden centre' }
-    address
     url { 'http://example.com' }
+
+    address
+
     after(:build) { |partner| partner.turfs = [create(:turf)] }
+    # image nil
   end
 end
--- test/models/calendar_test.rb
+++ test/models/calendar_test.rb
@@ -3,7 +3,27 @@
 require 'test_helper'
 
 class CalendarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @calendar = Calendar.new
+  end
+
+  test 'has required fields' do
+    # Must have a name and source URL
+    @calendar.save
+    refute @calendar.valid?
+    @calendar.update(name: "A name for the calendar")
+    refute @calendar.valid?
+    @calendar.update(source: "http://my-calendar.com")
+    assert @calendar.valid?
+
+    # Sources must be unique
+    @existing_calendar = create(:calendar)
+    @existing_calendar.update(source: "http://my-calendar.com")
+    refute @existing_calendar.valid?
+    assert_equal ["Calendar source is already in use"], @existing_calendar.errors[:source]
+  end
+
+  test 'gets a contact for each calendar' do
+    @calendar = Calendar.new(name: 'Test calendar', source: 'http://my-calendar.com')
+  end
 end
