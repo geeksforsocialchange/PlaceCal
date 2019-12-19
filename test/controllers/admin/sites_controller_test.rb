4b69c442edf09e5943d63bb8cd4e86322daab177
Dr Kim Foale <kim@gfsc.studio>
2019-12-19 16:24:36 +0000

    Update site policies

--- test/controllers/admin/sites_controller_test.rb
+++ test/controllers/admin/sites_controller_test.rb
@@ -5,7 +5,9 @@ require 'test_helper'
 class Admin::SitesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @site = create(:site)
+    @site_two = create(:site)
     @root = create(:root)
+    @site_admin = @site.site_admin
     @citizen = create(:user)
 
     host! 'admin.lvh.me'
@@ -14,11 +16,21 @@ class Admin::SitesControllerTest < ActionDispatch::IntegrationTest
   # Site Index
   #
   #   Show every Site for roots
+  #   Show just user's sites for non-roots
   #   Redirect everyone else to admin_root_url
 
   it_allows_access_to_index_for(%i[root]) do
     get admin_sites_url
     assert_response :success
+    # Will see both sites
+    assert_select 'tbody tr', count: 2
+  end
+
+  it_allows_access_to_index_for(%i[site_admin]) do
+    get admin_sites_url
+    assert_response :success
+    # Will just see their site
+    assert_select 'tbody tr', count: 1
   end
 
   it_denies_access_to_index_for(%i[citizen]) do
@@ -36,8 +48,20 @@ class Admin::SitesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  it_denies_access_to_new_for(%i[site_admin citizen]) do
+    get new_admin_site_url
+    assert_redirected_to admin_root_url
+  end
+
   it_allows_access_to_create_for(%i[root]) do
     assert_difference('Site.count') do
+      post admin_sites_url,
+           params: { site: attributes_for(:site) }
+    end
+  end
+
+  it_denies_access_to_create_for(%i[site_admin citizen]) do
+    assert_no_difference('Site.count') do
       post admin_sites_url,
            params: { site: attributes_for(:site) }
     end
@@ -48,12 +72,12 @@ class Admin::SitesControllerTest < ActionDispatch::IntegrationTest
   #   Allow roots to edit all places
   #   Everyone else, redirect to admin_root_url
 
-  it_allows_access_to_edit_for(%i[root]) do
+  it_allows_access_to_edit_for(%i[root site_admin]) do
     get edit_admin_site_url(@site)
     assert_response :success
   end
 
-  it_allows_access_to_update_for(%i[root]) do
+  it_allows_access_to_update_for(%i[root site_admin]) do
     patch admin_site_url(@site),
           params: { site: attributes_for(:site) }
     # Redirect to main partner screen
@@ -71,5 +95,13 @@ class Admin::SitesControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to admin_sites_url
+  end
+
+  it_denies_access_to_destroy_for(%i[site_admin citizen]) do
+    assert_no_difference('Site.count') do
+      delete admin_site_url(@site)
+    end
+
+    assert_redirected_to admin_root_url
   end
 end
