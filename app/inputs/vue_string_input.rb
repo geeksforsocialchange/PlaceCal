37ba7dbd5a36579de35188d372b35c0710ef8fdc
Dr Kim Foale <kim@gfsc.studio>
2020-01-10 13:14:38 +0000

    Pass the v-model value based on form element id to the form builder

--- app/inputs/vue_string_input.rb
+++ app/inputs/vue_string_input.rb
@@ -1,22 +1,14 @@
 # app/inputs/vue_string_input.rb
 
 class VueStringInput < SimpleForm::Inputs::StringInput
-  def merge_wrapper_options(options, wrapper_options)
-    if wrapper_options
-      wrapper_options = set_input_classes(wrapper_options)
-
-      wrapper_options.merge(options) do |key, oldval, newval|
-        case key.to_s
-        when "class"
-          Array(oldval) + Array(newval)
-        when "data", "aria", "v"
-          oldval.merge(newval)
-        else
-          newval
-        end
-      end
-    else
-      options
+  def input(wrapper_options = nil)
+    unless string?
+      input_html_classes.unshift("string")
+      input_html_options[:type] ||= input_type if html5?
     end
+
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+    merged_input_options[:'v-model'] = attribute_name
+    @builder.text_field(attribute_name, merged_input_options)
   end
 end
