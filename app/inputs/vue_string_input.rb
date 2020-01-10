ee6fdff451acb9d520e3fe984033d0cf678c3cca
Dr Kim Foale <kim@gfsc.studio>
2020-01-10 13:29:35 +0000

    Add possible code to make it pass form builder

--- app/inputs/vue_string_input.rb
+++ app/inputs/vue_string_input.rb
@@ -1,6 +1,19 @@
 # app/inputs/vue_string_input.rb
 
+module SimpleForm
+  module Components
+    module VueModel
+      def vue_model(wrapper_options = nil)
+        input_html_options[:'v-model'] ||= options[:'v-model']
+        nil
+      end
+    end
+  end
+end
+
 class VueStringInput < SimpleForm::Inputs::StringInput
+  enable :placeholder, :maxlength, :minlength, :pattern, :vue_model
+
   def input(wrapper_options = nil)
     unless string?
       input_html_classes.unshift("string")
@@ -8,7 +21,8 @@ class VueStringInput < SimpleForm::Inputs::StringInput
     end
 
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
-    merged_input_options[:'v-model'] = attribute_name
+    puts merged_input_options
+    # merged_input_options[:'v-model'] = attribute_name
     @builder.text_field(attribute_name, merged_input_options)
   end
 end
