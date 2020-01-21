86b00442b9efeea10fcf44288fd411a35e6fd0ad
Dr Kim Foale <kim@gfsc.studio>
2020-01-21 14:48:42 +0000

    Fix tests

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -6,6 +6,8 @@ class Calendar < ApplicationRecord
   include Validation
   extend Enumerize
 
+  CALENDAR_REGEX = /\A(?:(?:(https?|webcal)):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?\z/i
+
   self.inheritance_column = nil
 
   belongs_to :partner, optional: true
@@ -16,7 +18,7 @@ class Calendar < ApplicationRecord
   validates :place, presence: { if: :requires_default_location?,
                                 message: "can't be blank with this strategy" }
   validates :source, uniqueness: { message: 'calendar source already in use' },
-                     format: { with: URL_REGEX, message: 'not a valid URL' }
+                     format: { with: CALENDAR_REGEX, message: 'not a valid URL' }
 
   before_save :source_supported
 
