0627bee0744cbc5dc63fa1b235e449066fd04f27
GabAlexandria <gabagrant@gmail.com>
2020-03-15 22:57:30 -0400

    fix mailer test names

--- test/mailers/join_mailer_test.rb
+++ test/mailers/join_mailer_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-describe ContactMailer do
+describe JoinMailer do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
--- test/mailers/previews/join_mailer_preview.rb
+++ test/mailers/previews/join_mailer_preview.rb
@@ -1,4 +1,4 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
-class ContactMailerPreview < ActionMailer::Preview
+class JoinMailerPreview < ActionMailer::Preview
 
 end
