46f777efcf4884a391a347d9924a26b3c8ee90df
Dr Kim Foale <kim@gfsc.studio>
2020-03-18 14:17:11 +0000

    Convert steps to component

--- app/components/steps/steps_component.rb
+++ app/components/steps/steps_component.rb
@@ -2,6 +2,7 @@
 
 # app/components/steps/steps_component.rb
 class StepsComponent < MountainView::Presenter
-
-
+  property :start_message, default: "Nothing is connected! People don't know where to go."
+  property :end_message, default: "Problem solved: everyone's connected and it's easy to find out what's happening!"
+  property :steps
 end
