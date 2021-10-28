193f611eb8e01e99dbc7b17cfc50af2e5bfb1590
Alexandria <alexandria@gfsc.studio>
2021-10-28 15:09:55 +0100

    Fix travis deploying to production

--- scripts/production.sh
+++ scripts/production.sh
@@ -2,6 +2,6 @@ eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 ./production-deploy.key # this key should have push access
 ssh-add ./production-deploy.key
 ssh-keyscan placecal.org >> ~/.ssh/known_hosts
-git remote add deploy dokku@prod.placecal.org:placecal
+git remote add deploy dokku@placecal.com:placecal
 git config --global push.default simple
 git push deploy production:master
