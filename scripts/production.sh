888acb3ec1e11ba850ac49074a89622cdcdf2b51
Alexandria <alexandria@gfsc.studio>
2021-10-28 15:39:04 +0100

    Fix production.sh not having placecal.com in known_hosts

--- scripts/production.sh
+++ scripts/production.sh
@@ -1,7 +1,7 @@
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 ./production-deploy.key # this key should have push access
 ssh-add ./production-deploy.key
-ssh-keyscan placecal.org >> ~/.ssh/known_hosts
+ssh-keyscan placecal.com >> ~/.ssh/known_hosts
 git remote add deploy dokku@placecal.com:placecal
 git config --global push.default simple
 git push deploy production:master
