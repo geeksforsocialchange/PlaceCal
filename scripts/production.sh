90e9d44fc4c23832a5bb47cf629df0b6066a11bb
Alexandria <alexandria@gfsc.studio>
2021-11-15 15:29:50 +0000

    Deploy: placecal.com -> placecal.org

--- scripts/production.sh
+++ scripts/production.sh
@@ -2,7 +2,7 @@ eval "$(ssh-agent -s)" #start the ssh agent
 echo "$CI_PRODUCTION_KEY" > ./production-deploy.key
 chmod 600 ./production-deploy.key # this key should have push access
 ssh-add ./production-deploy.key
-ssh-keyscan placecal.com >> ~/.ssh/known_hosts
-git remote add deploy dokku@placecal.com:placecal
+ssh-keyscan placecal.org >> ~/.ssh/known_hosts
+git remote add deploy dokku@placecal.org:placecal
 git config --global push.default simple
 git push deploy production:main
