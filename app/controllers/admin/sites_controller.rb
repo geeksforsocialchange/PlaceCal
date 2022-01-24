10f26818ba7b0fbcd882c3c2c28dfc60dfd2e02f
Alexandria <alexandria@gfsc.studio>
2022-01-24 19:15:30 +0000

    Not sure why rebasing removed the policy_scope change. Otherwise this is 99% ready lol

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -58,7 +58,7 @@ module Admin
     end
 
     def set_variables_for_sites_neighbourhoods_selection
-      @all_neighbourhoods = policy_scope(Neighbourhood).order(:name).where(unit: 'ward')
+      @all_neighbourhoods = policy_scope(Neighbourhood).order(:name)
       begin
         set_site
       rescue ActiveRecord::RecordNotFound
