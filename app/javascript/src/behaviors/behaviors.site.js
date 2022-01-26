423253d8d141a413eac6cfd09a47c2f4f8f00d74
Alexandria <alexandria@gfsc.studio>
2022-01-26 14:23:58 +0000

    Order js names; Style sites neighbourhoods subform; Attach select2 to cocoon (does not work); Rename button

--- app/javascript/src/behaviors/behaviors.site.js
+++ app/javascript/src/behaviors/behaviors.site.js
@@ -2,6 +2,12 @@ jQuery.extend(Behaviors, {
   site: {
     form: {
       init: function() {
+        // Attach select2 to all future select2 nodes
+        $(".sites_neighbourhoods").bind("cocoon:after-insert", function (_, entry) {
+          entry.children('.select2').first.select2({ multiple: false });
+        });
+
+        // Attach select2 to the current select2 nodes
         $( ".select2" ).select2({
           multiple: false
         });
