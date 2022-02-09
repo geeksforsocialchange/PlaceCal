d6becd35e4c2748bab0a5ea40452c1abfc5e95c4
Alexandria | Aster <92374839+alexandria-gfsc@users.noreply.github.com>
2022-02-09 16:56:47 +0000

    Fix partners page not showing descendant partners (#924)

--- test/integration/partners_integration_test.rb
+++ test/integration/partners_integration_test.rb
@@ -7,6 +7,7 @@ class PartnersIntegrationTest < ActionDispatch::IntegrationTest
     # Create a default site and a neighbourhood one
     @default_site = create_default_site
     @neighbourhood_site = create(:site_local)
+    @region_site = create(:site_local)
 
     # Create one set of partners for the default site
     @default_site_partners = create_list :partner, 5
@@ -23,6 +24,14 @@ class PartnersIntegrationTest < ActionDispatch::IntegrationTest
       partner.address.update(neighbourhood: @neighbourhood2)
     end
     @neighbourhood_site.neighbourhoods << @neighbourhood2
+
+    # Set up a site bound to a region, and partners bound to a region descendant
+    @region_site_partners = create_list :partner, 5
+    @neighbourhood3 = create(:neighbourhood)
+    @region_site_partners.each do |partner|
+      partner.address.update(neighbourhood: @neighbourhood3)
+    end
+    @region_site.neighbourhoods << @neighbourhood3.region
   end
 
   test 'placecal partners page shows all partners and relevant local info' do
@@ -47,5 +56,17 @@ class PartnersIntegrationTest < ActionDispatch::IntegrationTest
     # Ensure title/summary description is displayed
     assert_select '.preview__header', text: @neighbourhood_site_partners.first.name
     assert_select '.preview__details', text: @neighbourhood_site_partners.first.summary
+  end
+
+  test 'region site page shows descendent partners' do
+    get "http://#{@region_site.slug}.lvh.me/partners"
+    assert_response :success
+    assert_select 'title', count: 1, text: "Partners in your area | #{@region_site.name}"
+    assert_select 'div.hero h4', text: "Neighbourhood's Community Calendar"
+    assert_select 'div.hero h1', text: 'Partners in your area'
+    assert_select 'ul.partners li', 5
+    # Ensure title/summary description is displayed
+    assert_select '.preview__header', text: @region_site_partners.first.name
+    assert_select '.preview__details', text: @region_site_partners.first.summary
   end
 end
