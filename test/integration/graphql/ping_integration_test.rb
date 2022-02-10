d44d880c9000e6fd7ec94171f4385be702895495
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-02-10 13:56:48 +0000

    GraphQL partner endpoints (#927)
    * Partner Query type and responses
    * Basic tests covering Partner Graph API

---
+++ test/integration/graphql/ping_integration_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class GraphQLPingTest < ActionDispatch::IntegrationTest
+  test 'it returns a string' do
+    query_string = <<-GRAPHQL
+      query {
+        ping
+      }
+    GRAPHQL
+
+    result = PlaceCalSchema.execute(query_string)
+    data = result['data']
+    assert data.has_key?('ping')
+
+    assert data['ping'] =~ /^Hello World! The time is \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/
+  end
+end
