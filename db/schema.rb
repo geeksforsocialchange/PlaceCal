995d708eb03474aaf04f4f8cd871eecc928e7408
Genevieve Clifford <genevieve@becquerel.me>
2022-02-16 18:24:39 +0000

    Changed Articles: rename :description -> :body, rename :published -> :published_at, change :published_at to :datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_16_162803) do
+ActiveRecord::Schema.define(version: 2022_02_16_181333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 2022_02_16_162803) do
 
   create_table "articles", force: :cascade do |t|
     t.text "title"
-    t.text "description"
-    t.date "published"
+    t.text "body"
+    t.datetime "published_at"
     t.boolean "is_draft", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
