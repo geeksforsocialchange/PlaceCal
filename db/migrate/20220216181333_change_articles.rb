3fafc34387ed181ea5a0121b8c40d7d15eb91a3d
Genevieve Clifford <genevieve@becquerel.me>
2022-02-17 12:00:53 +0000

    remove articles migrations

--- db/migrate/20220216181333_change_articles.rb
+++
@@ -1,9 +0,0 @@
-class ChangeArticles < ActiveRecord::Migration[6.1]
-  def change
-    change_table :articles do |t|
-      t.rename :description, :body
-      t.rename :published, :published_at
-      t.change :published_at, :datetime
-    end
-  end
-end
