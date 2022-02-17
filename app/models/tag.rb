45ff85335e5f0242afc73f57b31d77e7e5cffdad
Alexandria <alexandria@gfsc.studio>
2022-02-17 05:48:14 +0000

    Add edit_permission field to tags, adjust interface/datatables
    - Add edit_permission to schema
    - Add description validation, tags enum, friendly id to tags model
    - Correct friendly id in tags controller
    - Add tags edit permission to datatables, edit form

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
 class Tag < ApplicationRecord
-  self.table_name = 'tags'
+  extend FriendlyId
+  extend Enumerize
+
+  friendly_id :name, use: :slugged
+
+  self.table_name = 'tags' # Maybe we can remove this? Tag should automagically railsify to tags right?
 
   has_and_belongs_to_many :users
   has_and_belongs_to_many :partners
 
   validates :name, :slug, presence: true
   validates :name, :slug, uniqueness: true
+  validates :description,
+            length: {
+              maximum: 200,
+              too_long: 'maximum length is 200 characters'
+            }
+  validates :edit_permission, presence: true
+
+  enumerize :edit_permission,
+            in: %i[root all],
+            default: :root
 end
