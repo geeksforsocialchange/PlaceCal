a140a1679eb721446b351c5e76dc8debde67e8e6
Alexandria | Aster <92374839+alexandria-gfsc@users.noreply.github.com>
2022-02-17 10:27:08 +0000

    Add edit_permission field to tags, adjust interface/datatables (#966)
    - Add edit_permission to schema
    - Add description validation, tags enum, friendly id to tags model
    - Correct friendly id in tags controller
    - Add tags edit permission to datatables, edit form

--- test/factories/tag.rb
+++ test/factories/tag.rb
@@ -6,5 +6,12 @@ FactoryBot.define do
       "Hulme #{n}"
     end
     slug { name.parameterize }
+    description { 'I am a tag' }
+    edit_permission { 'root' }
+
+    factory :tag_perms_all, class: :tag do
+      description { 'I am a public tag' }
+      edit_permission { 'public' }
+    end
   end
 end
