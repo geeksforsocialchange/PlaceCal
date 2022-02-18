19d066509210e7f89e00d491507d0407e1f8195b
Genevieve Clifford <genevieve@becquerel.me>
2022-02-18 15:00:20 +0000

    updated factory for Article

--- test/factories/articles.rb
+++ test/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyText" }
-    body { "MyText" }
+    title { "No D&D session this week!" }
+    body { "We regret to inform you that this week's D&D session has been cancelled, the DM's dog has eaten a key NPC's character sheet, bad Fido! We'll be back in a fortnight!" }
     published_at { "2022-02-16" }
     is_draft { true }
   end
