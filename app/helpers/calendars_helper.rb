94803af2a3f117a74537dfad75c941fef4b48bcc
Alexandria <alexandria@gfsc.studio>
2022-02-21 14:22:27 +0000

    Fix strategy_label location

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -21,6 +21,29 @@ module CalendarsHelper
     end
   end
 
+  def strategy_label val
+    case val.second
+    when 'event'
+      '<strong>Event</strong>: ' \
+      "Get the location of this event from the address field on the source event. " \
+      "This is for area calendars, or organisations with no solid base.".html_safe
+    when 'place'
+      "<strong>Default location</strong>: " \
+      "Every event is in one location (set below). The address field on the source calendar is ignored.".html_safe
+    when 'room_number'
+      "<strong>Room Number</strong>: " \
+      "Every event is on one location (set below), and the address field is used " \
+      "to store a room number.".html_safe
+    when 'event_override'
+      "<strong>Event Override</strong>: " \
+      "Every event is in one location (set below), unless the address field " \
+      "is set to another location".html_safe
+    else
+      val
+    end
+  end
+
+
   def display_time_since(date)
     if DateTime.current.to_date == date.to_date
       'Today'
--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -41,30 +41,6 @@ class Calendar < ApplicationRecord
     %i[place room_number event_override].include? strategy.to_sym
   end
 
-  class << self
-    def strategy_label val
-      case val.second
-      when 'event'
-        '<strong>Event</strong>: ' \
-        "Get the location of this event from the address field on the source event. " \
-        "This is for area calendars, or organisations with no solid base.".html_safe
-      when 'place'
-        "<strong>Default location</strong>: " \
-        "Every event is in one location (set below). The address field on the source calendar is ignored.".html_safe
-      when 'room_number'
-        "<strong>Room Number</strong>: " \
-        "Every event is on one location (set below), and the address field is used " \
-        "to store a room number.".html_safe
-      when 'event_override'
-        "<strong>Event Override</strong>: " \
-        "Every event is in one location (set below), unless the address field " \
-        "is set to another location".html_safe
-      else
-        val
-      end
-    end
-  end
-
   # Output constant for event import date limit
   def self.import_up_to
     1.year.from_now
