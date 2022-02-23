2813abbbd43788dfa209cc0c03baa0b7b86f783b
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-02-23 16:14:52 +0000

    Minor test code clean up

--- test/models/partner_test.rb
+++ test/models/partner_test.rb
@@ -230,36 +230,28 @@ class PartnerAddressOrServiceAreaPermissionsTest < ActiveSupport::TestCase
   setup do
     @user = create(:user)
     @user_neighbourhood =  neighbourhoods(:one)
+    
     @user.neighbourhoods << @user_neighbourhood
+
+    @new_partner = build(
+      :partner, 
+      address: nil,
+      accessed_by_user: @user
+    )
   end
 
-  test "verify: address in user ward" do
-    new_partner = build(
-      :partner, 
-      address: create(
-        :address,
-        neighbourhood: @user_neighbourhood
-      )
-    )
+  test "valid if address is in user ward" do
+    @new_partner.address = create(:address, neighbourhood: @user_neighbourhood)
+    @new_partner.save!
 
-    new_partner.accessed_by_user = @user
-    new_partner.save!
-
-    assert new_partner.valid?
+    assert @new_partner.valid?
   end
 
   test "verify: with service area in user neighbourhoods" do
-    
-    new_partner = build(
-      :partner,
-      address: nil
-    )
+    @new_partner.service_area_neighbourhoods << @user_neighbourhood
+    @new_partner.save!
 
-    new_partner.service_area_neighbourhoods << @user_neighbourhood
-    new_partner.accessed_by_user = @user
-    new_partner.save!
-
-    assert new_partner.valid?
+    assert @new_partner.valid?
   end
 
   test "verify: with service area contained within users neighbourhood subtrees" do
@@ -268,16 +260,19 @@ class PartnerAddressOrServiceAreaPermissionsTest < ActiveSupport::TestCase
 
     @user.neighbourhoods << parent_neighbourhood
 
-    new_partner = build(
-      :partner,
-      address: nil
-    )
+    @new_partner.service_area_neighbourhoods << child_neighbourhood
+    @new_partner.save!
 
-    new_partner.service_area_neighbourhoods << child_neighbourhood
-    new_partner.accessed_by_user = @user
-    new_partner.save!
+    assert @new_partner.valid?
+  end
 
-    assert new_partner.valid?
+  test "with a service area not in user's ward set" do
+    other_neighbourhood = neighbourhoods(:two)
+
+    @new_partner.service_area_neighbourhoods << @user_neighbourhood
+    @new_partner.service_area_neighbourhoods << other_neighbourhood
+
+    assert @new_partner.valid? == false, 'Users cannot create service areas outside of their neighbourhoods'
   end
 end
 
