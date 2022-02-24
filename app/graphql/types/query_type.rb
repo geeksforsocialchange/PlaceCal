51a5d5e3ac27803d7e636b38dfe22b684bb57755
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-02-24 17:03:01 +0000

    Attempt at tidying up GraphQL query class
    Some isolation of concerns

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,72 @@
 module Types
-  class QueryType < Types::BaseObject
+  ID = GraphQL::Types::ID
+
+  module PartnerQueries
+    def self.included(klass)
+      klass.field :partner, PartnerType, "Find Partner by ID" do
+        argument :id, ID
+      end
+
+      klass.field :all_partners, [PartnerType]
+    end
+
+    def partner(id:)
+      Partner.find(id)
+    end
+
+    def all_partners
+      Partner.all
+    end
+  end
+
+  module EventQueries
+    def self.included(klass)
+      klass.field :event, EventType, "Find Event by ID" do
+        argument :id, ID
+      end
+
+      klass.field :all_events, [EventType]
+    end
+
+    def event(id:)
+      Event.find(id)
+    end
+
+    def all_events
+      Event.all
+    end
+  end
+
+  module SiteQueries
+    def self.included(klass)
+      klass.field :site, SiteType, "Find Site by ID" do
+        argument :id, ID
+      end
+
+      klass.field :all_sites, [SiteType]
+    end
+
+    def site(id:)
+      Site.find(id)
+    end
+
+    def all_sites
+      Site.all
+    end
+  end
+
+  module MiscQueries
+    def self.included(klass)
+      klass.field :ping, String, null: false, description: "Ping server"
+    end
+
+    def ping
+      "Hello World! The time is #{Time.now}"
+    end
+  end
+
+  class QueryType < Types::BaseObject 
+
     # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     # include GraphQL::Types::Relay::HasNodeField
     # include GraphQL::Types::Relay::HasNodesField
@@ -9,57 +76,9 @@ module Types
 
     # field :fetch_partners, resolver: Beans # Queries::FetchPartners
 
-    field :partner, PartnerType, "Find Partner by ID" do
-      argument :id, ID
-    end
-
-    field :all_partners, [PartnerType]
-
-    def partner(id:)
-      Partner.find(id)
-    end
-
-    def all_partners
-      Partner.all
-    end
-
-    ####
-
-    field :event, EventType, "Find Event by ID" do
-      argument :id, ID
-    end
-
-    field :all_events, [EventType]
-
-    def event(id:)
-      Event.find(id)
-    end
-
-    def all_events
-      Event.all
-    end
-
-    ####
-
-    field :site, SiteType, "Find Site by ID" do
-      argument :id, ID
-    end
-
-    field :all_sites, [SiteType]
-
-    def site(id:)
-      Site.find(id)
-    end
-
-    def all_sites
-      Site.all
-    end
-
-    ####
-    field :ping, String, null: false, description: "Ping server"
-
-    def ping
-      "Hello World! The time is #{Time.now}"
-    end
+    include PartnerQueries
+    include EventQueries
+    include SiteQueries
+    include MiscQueries
   end
 end
