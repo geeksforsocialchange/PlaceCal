28d9f1ffede76123f0e7fda162355c2e6671c84a
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-03-02 15:43:25 +0000

    Bypass rails authentication for graphql endpoint (#1008)

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,4 +1,7 @@
 class GraphqlController < ApplicationController
+  
+  skip_before_action :verify_authenticity_token
+
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
