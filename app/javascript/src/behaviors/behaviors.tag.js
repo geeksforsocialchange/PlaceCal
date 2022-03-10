556b65e17832541de596cce1176d50a4570b8971
Dr Kim Foale <kim@gfsc.studio>
2022-03-10 17:33:22 +0000

    Patch activestorage

--- app/javascript/src/behaviors/behaviors.tag.js
+++ app/javascript/src/behaviors/behaviors.tag.js
@@ -1,8 +1,10 @@
 jQuery.extend(Behaviors, {
   tag: {
     form: {
-      init: function() {
-        $('.select2').each(function () { $(this).select2({ multiple: true }); });
+      init: function () {
+        $('.select2').select2({
+          multiple: true
+        });
       }
     }
   }
