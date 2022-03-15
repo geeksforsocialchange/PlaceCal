bd6e2956089c1853fed1b0880783e7378682f574
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-03-15 13:57:55 +0000

    Event filter by fromDate to toDate

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -26,6 +26,11 @@ module Types
       end
 
       klass.field :event_connection, Types::EventType.connection_type
+
+      klass.field :event_by_filter, [Types::EventType] do
+        argument :from_date, String, required: false
+        argument :to_date, String, required: false
+      end
     end
 
     def event(id:)
@@ -34,6 +39,54 @@ module Types
 
     def event_connection(**args)
       Event.sort_by_time.all
+    end
+
+    def event_by_filter(**args)
+      query = Event.sort_by_time
+      from_date = DateTime.now
+
+      if args[:from_date].present?
+        if args[:from_date] =~ /^\s*(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})/
+          from_date = DateTime.new(
+            $1.to_i, # year
+            $2.to_i, # month
+            $3.to_i, # day
+            $4.to_i, # hour
+            $5.to_i, # minute
+            0,       # seconds
+          )
+        else
+          raise GraphQL::ExecutionError, "fromDate not in 'YYYY-MM-DD HH:MM' format"
+        end
+      end
+
+      query = query.where('dtstart >= ?', from_date)
+
+      if args[:to_date].present?
+        if args[:to_date] =~ /^\s*(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})/
+          to_date = DateTime.new(
+            $1.to_i, # year
+            $2.to_i, # month
+            $3.to_i, # day
+            $4.to_i, # hour
+            $5.to_i, # minute
+            0,       # seconds
+          )
+
+          if to_date <= from_date
+            raise GraphQL::ExecutionError, "toDate is before fromDate"
+          end
+
+        else
+          raise GraphQL::ExecutionError, "toDate not in 'YYYY-MM-DD HH:MM' format"
+        end
+
+        query = query.where('dtstart < ?', to_date)
+      end
+
+      # query.where('dtend < ?', args[:end_date]) if args[:end_date]
+
+      query.all
     end
   end
 
--- test/integration/graphql/event_integration_test.rb
+++ test/integration/graphql/event_integration_test.rb
@@ -45,6 +45,7 @@ class GraphQLEventTest < ActionDispatch::IntegrationTest
     GRAPHQL
 
     result = PlaceCalSchema.execute(query_string)
+    # puts JSON.pretty_generate(result.as_json)
     data = result['data']
 
     assert data.has_key?('eventConnection'), 'result is missing key `eventConnection`'
@@ -104,5 +105,127 @@ class GraphQLEventTest < ActionDispatch::IntegrationTest
     assert data_event.has_key?('endDate'), 'missing endDate'
     assert data_event.has_key?('address'), 'missing address'
     assert data_event.has_key?('organizer'), 'missing organizer'
+  end
+
+  # the filter tests
+
+  def build_time_events(now_time)
+
+    # events in the past
+    time = now_time - 100.days
+    5.times do
+      @partner.events.create!(
+        dtstart: time,
+        summary: "past: An event summary",
+        description: 'Longer text covering the event in more detail',
+        address: @address
+      )
+      time += 1.days
+    end
+
+    # events in the near future
+    time = now_time + 1.days
+    5.times do
+      @partner.events.create!(
+        dtstart: time,
+        summary: "present: An event summary",
+        description: 'Longer text covering the event in more detail',
+        address: @address
+      )
+      time += 1.days
+    end
+
+    # events in the far future
+    time = now_time + 100.days
+    5.times do
+      @partner.events.create!(
+        dtstart: time,
+        summary: "future: An event summary",
+        description: 'Longer text covering the event in more detail',
+        address: @address
+      )
+      time += 1.days
+    end
+  end
+
+  test 'returns events from today' do
+    now_time = DateTime.new(1990, 1, 1, 0, 0, 0)
+    build_time_events now_time
+
+    DateTime.stub :now, now_time do
+
+      query_string = <<-GRAPHQL
+      query {
+        eventByFilter {
+          id
+          name
+          startDate
+        }
+      }
+      GRAPHQL
+
+      result = PlaceCalSchema.execute(query_string)
+      assert result.has_key?('errors') == false, 'errors are present'
+
+      data = result['data']
+      assert data.has_key?('eventByFilter'), 'Data structure does not contain event key'
+
+      events = data['eventByFilter']
+      assert events.length == 10, 'was expecting only events in the future'
+    end
+  end
+
+  test 'returns events from a given point in time' do
+    now_time = DateTime.new(1990, 1, 1, 0, 0, 0)
+    build_time_events now_time
+
+    DateTime.stub :now, now_time do
+
+      query_string = <<-GRAPHQL
+      query {
+        eventByFilter(fromDate: "1985-01-01 00:00") {
+          id
+          name
+          startDate
+        }
+      }
+      GRAPHQL
+
+      result = PlaceCalSchema.execute(query_string)
+      assert result.has_key?('errors') == false, 'errors are present'
+
+      data = result['data']
+      assert data.has_key?('eventByFilter'), 'Data structure does not contain event key'
+
+      events = data['eventByFilter']
+      assert events.length == 15, 'was expecting to see all events'
+    end
+  end
+
+  test 'returns events in a given range when blocked out' do
+    now_time = DateTime.new(1990, 1, 1, 0, 0, 0)
+    build_time_events now_time
+
+    DateTime.stub :now, now_time do
+
+      query_string = <<-GRAPHQL
+      query {
+        eventByFilter(fromDate: "1990-01-01 00:00", toDate: "1990-03-01 00:00") {
+          id
+          name
+          startDate
+        }
+      }
+      GRAPHQL
+
+      result = PlaceCalSchema.execute(query_string)
+      assert result.has_key?('errors') == false, 'errors are present'
+
+      data = result['data']
+      assert data.has_key?('eventByFilter'), 'Data structure does not contain event key'
+
+      events = data['eventByFilter']
+      assert events.length == 5, 'was expecting to see only some future events'
+    end
   end
 end
