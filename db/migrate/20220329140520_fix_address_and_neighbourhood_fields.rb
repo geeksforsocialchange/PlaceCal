a6a4cfd6b6beac17212ba6035acd213eb88f8b41
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-03-30 15:07:26 +0100

    Fix data migration snafu

--- db/migrate/20220329140520_fix_address_and_neighbourhood_fields.rb
+++ db/migrate/20220329140520_fix_address_and_neighbourhood_fields.rb
@@ -1,17 +1,5 @@
 class FixAddressAndNeighbourhoodFields < ActiveRecord::Migration[6.1]
   def change
-    Address.transaction do
-      Address.all.each do |address|
-        address.postcode = address.postcode
-        address.save!
-      end
-    end
-
-    Neighbourhood.transaction do
-      Neighbourhood.all.each do |neighbourhood|
-        neighbourhood.name_abbr = neighbourhood.name_abbr
-        neighbourhood.save!
-      end
-    end
+    # removed code as this type of fix is better performed elsewhere
   end
 end
