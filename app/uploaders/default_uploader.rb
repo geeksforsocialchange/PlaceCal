b88508fd1f150f104c47c8b9526ded76348b69a8
Dr Kim Foale <kim@gfsc.studio>
2022-04-07 13:35:33 +0100

    Rejig article form, fix image upload file size
    Fixes #1116 and #1128

---
+++ app/uploaders/default_uploader.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class DefaultUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # This is limited by Nginx
+  def size_range
+    1.byte..1.megabytes
+  end
+end
--- app/uploaders/hero_image_uploader.rb
+++ app/uploaders/hero_image_uploader.rb
@@ -1,29 +1,7 @@
 # frozen_string_literal: true
 
 # app/uploaders/hero_image_uploader.rb
-class HeroImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
+class HeroImageUploader < DefaultUploader
   process resize_to_fit: [2260, 700]
 
   version :standard do
