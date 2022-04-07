102121caec447d46642f2601848d336b8b7c3065
Dr Kim Foale <kim@gfsc.studio>
2022-04-07 15:23:50 +0100

    Rejig article form, fix image upload file size (#1132)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,21 +1,7 @@
 # frozen_string_literal: true
 
 # app/uploaders/image_uploader.rb
-class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
+class ImageUploader < DefaultUploader
   # Process files as they are uploaded:
   process resize_to_fit: [1200, 1200]
 
