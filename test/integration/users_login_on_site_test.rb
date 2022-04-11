0e6d6a8b26f230991f50968aba79e5bba89a1118
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-04-11 14:53:14 +0100

    Some tests covering redirect logic
    - non-logged-in-users are redirected to base site if not on base site
    - when loggin in users are redirected to the admin site

---
+++ test/integration/users_login_on_site_test.rb
@@ -0,0 +1,17 @@
+
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UsersLoginOnSiteTest < ActionDispatch::IntegrationTest
+
+  setup do
+    create_default_site
+  end
+
+  test 'redirects to base site' do
+    get 'http://default-site.lvh.me/users/sign_in'
+
+    assert_redirected_to 'http://lvh.me/users/sign_in'
+  end
+end
