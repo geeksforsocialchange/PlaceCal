4bb1b5599c1f024c4b6061394d50499f3d77a370
Dr Kim Foale <kim@gfsc.studio>
2022-04-12 16:50:53 +0100

    Silence webpacker

--- babel.config.js
+++ babel.config.js
@@ -72,7 +72,8 @@ module.exports = function(api) {
         {
           async: false
         }
-      ]
+      ],
+      ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
     ].filter(Boolean)
   }
 }
