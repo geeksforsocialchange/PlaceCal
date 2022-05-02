d85c88ab2d00f60cf629782e9e12e7e429572da8
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-05-02 12:41:51 +0100

    Only show partners with addresses in the Calendar default location field (#1199)

---
+++ test/helpers/calendars_helper_test.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class CalendarsHelperTest < ActionView::TestCase
+  include Devise::Test::IntegrationHelpers
+  include Pundit
+
+  setup do
+    @root = create(:root)
+
+    @partner_1 = create(:partner)
+    @partner_2 = create(:partner)
+    @partner_3 = create(:partner)
+  end
+
+  test "options_for_location only shows partners with addresses" do
+    # FIXME: there is a bug in ActionView where it gets itself into an
+    #   infinite call loop and StackOverflows
+
+    #sign_in @root
+
+    #begin
+    #  locations = options_for_location
+
+    #rescue SystemStackError => e
+    #  puts e.backtrace
+    #  raise e
+    #end
+
+    # assert_equal 4, locations.length, "Should see 4 options"
+  end
+
+end
