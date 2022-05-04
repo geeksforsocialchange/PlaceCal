5ec50146a702c40523d36135f67df0372215737d
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-05-04 14:48:30 +0100

    Streamline messy code in calendar importer

--- app/jobs/calendar_importer_job.rb
+++ app/jobs/calendar_importer_job.rb
@@ -1,41 +1,46 @@
 class CalendarImporterJob < ApplicationJob
   queue_as :default
 
+  def calendar
+    @calendar ||= Calendar.find(@calendar_id)
+  end
+
   # Imports all events from a given calendar
   # @param calendar_id [int] The ID of the Calendar object to import from
   # @param from_date [Date] The Date from which to import from
   def perform(calendar_id, from_date, force_import)
-    calendar = Calendar.find(calendar_id)
+    @calendar_id = calendar_id
 
-    name = calendar.name
-    id = calendar.id
-    place = calendar.place.try(:name) ? " for #{calendar.place.try(:name)}" : ''
-    force_import = force_import ? ' (Forced)' : ''
-    puts "Importing events for calendar #{name} (ID #{id})#{place}#{force_import}"
+    print "Importing events for calendar #{calendar.name} (ID #{calendar.id})"
+    print " for place #{calendar.place.name} (ID #{calendar.place.id})" if calendar.place
+    print " is forced" if force_import
+    print "\n"
 
     # calendar.import_events(from)
     CalendarImporter::CalendarImporterTask.new(calendar, from_date, force_import).run
+
   rescue CalendarImporter::EventResolver::Problem => e
-    error = "Could not automatically import data for calendar #{calendar.name} (id #{calendar_id}):  #{e}"
-    backtrace = e.backtrace[...6]
+    report_error e, "Could not automatically import data"
 
-    calendar.critical_import_failure e
-    puts error, backtrace
-    Rollbar.error error, { exception_type: 'EventResolver::Problem', backtrace: backtrace }
-  rescue CalendarImporter::CalendarImporter::InaccessibleFeed, CalendarImporter::CalendarImporter::UnsupportedFeed => e
-    error = "Calendar has inaccessible / unsupported feed #{calendar.name} (id #{calendar_id}):  #{e}"
-    backtrace = e.backtrace[...6]
+  rescue CalendarImporter::CalendarImporter::UnsupportedFeed => e
+    report_error e, "Calendar has unsupported feed URL"
 
-    calendar.critical_import_failure e
-    puts error, backtrace
-    Rollbar.error error, { exception_type: 'unsupported/inaccessible', backtrace: backtrace }
+  rescue CalendarImporter::CalendarImporter::InaccessibleFeed => e
+    report_error e, "Calendar has inaccessible feed URL"
+
   rescue StandardError => e
-    # TODO: Inform admin(s) when this fails
-    error = "Could not automatically import data for calendar #{calendar.name} (id #{calendar_id}):  #{e}"
+    report_error e, "Could not automatically import data"
+    raise e unless Rails.env.production?
+  end
+
+  private
+
+  def report_error(e, message)
+    full_message = "#{message} for calendar #{calendar.name} (id #{calendar.id}):  #{e}"
     backtrace = e.backtrace[...6]
 
-    calendar.critical_import_failure error
-    puts error, backtrace
-    Rollbar.error error, { exception_type: 'StandardError', backtrace: backtrace }
+    calendar.critical_import_failure full_message
+    puts full_message, backtrace if Rails.env.dev?
+    Rollbar.error error, { exception_type: e.class.name, backtrace: backtrace }
   end
 end
