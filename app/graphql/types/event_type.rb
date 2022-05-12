4f1e6cedc6a64f19503a092d975ceeacf73ce310
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-05-12 16:55:14 +0100

    Event publisherUrl field on GraphQL EventType

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -2,13 +2,13 @@ module Types
   class EventType < Types::BaseObject
     description 'An event represents a date in time that a partner operates their service, meetup, workout or class etc'
 
-    field :id, ID, 
+    field :id, ID,
       null: false,
       description: 'Our internal ID for further querying'
 
-    # Summary and name are aliases, this is left as a convenience 
+    # Summary and name are aliases, this is left as a convenience
     #   for people used to iCal format
-    field :name, String, 
+    field :name, String,
       method: :summary,
       description: 'An alias for `summary`'
 
@@ -18,21 +18,25 @@ module Types
     field :description, String,
       description: 'A longer text about this event covering more detail'
 
-    field :startDate, GraphQL::Types::ISO8601DateTime, 
-      method: :dtstart, 
+    field :startDate, GraphQL::Types::ISO8601DateTime,
+      method: :dtstart,
       null: false,
       description: 'The date (and time) when this event begins'
 
-    field :endDate, GraphQL::Types::ISO8601DateTime, 
+    field :endDate, GraphQL::Types::ISO8601DateTime,
       method: :dtend,
       description: 'The end date of this event if the event goes on for more than one day (i.e. a conference weekend)'
 
     field :address, AddressType,
       description: 'The address where this event will take place'
 
-    field :organizer, PartnerType, 
+    field :organizer, PartnerType,
       method: :partner,
       description: 'The organising partner of this event'
+
+    field :publisherUrl, String,
+      method: :publisher_url,
+      description: 'The URL of the partners original page'
 
   end
 end
