7730817880379907c67d7376b23dd3efb5d1dc09
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-06-13 09:59:41 +0100

    Front end uses pre-rendered HTML

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,16 +16,6 @@ class StrongParametersFormBuilder < SimpleForm::FormBuilder
 end
 
 module ApplicationHelper
-  def markdown(source)
-    if source
-      auto_link Kramdown::Document.new(source).to_html.html_safe do |text|
-        truncate(text, length: 30)
-      end
-    else
-      ''
-    end
-  end
-
   def user_policy
     UserPolicy.new(current_user, nil)
   end
