c3d8869e65c8d6192438eff80491378fbe1164b7
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-06-16 14:25:05 +0100

    Basic news summary on for article index page (#1320)

---
+++ test/helpers/articles_helper_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ArticlesHelperTest < ActionView::TestCase
+  test 'article_summary_text returns summary text' do
+    article = build(:article)
+
+    # handles null value
+    article.body = nil
+    output = article_summary_text(article)
+    assert_equal '', output
+
+    # handles short text
+    article.body = 'This is a body text'
+    output = article_summary_text(article)
+    assert_equal 19, output.length
+
+    # trims long text
+    article.body = 'a' * 250
+    output = article_summary_text(article)
+    assert_equal 200, output.length
+  end
+end
