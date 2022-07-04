41cc254fe44e95c3f7ae2496fd7971fbb5f7891e
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-07-04 15:34:20 +0100

    Don't show root users the no-content warning
    When logged in as a root user (who has no content)
    and on the home dashboard page
    then don't show the no content nag box

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,6 +15,7 @@ module UsersHelper
   end
 
   def user_has_no_rights?(user)
+    return false if user.root?
     return false if user.tag_admin?
     return false if user.neighbourhood_admin?
     return false if user.partner_admin?
