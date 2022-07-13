b2181d8b73a13908369140475e632aa160ee91a9
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-07-13 10:25:53 +0100

    Reversibility of DB migration
    Removing facebook columns from user can now be re-instated in
    db:rollback

--- db/migrate/20220711161804_remove_facebook_fields.rb
+++ db/migrate/20220711161804_remove_facebook_fields.rb
@@ -1,7 +1,7 @@
 class RemoveFacebookFields < ActiveRecord::Migration[6.1]
   def change
-    remove_column :partners, :facebook_link
-    remove_column :users, :facebook_app_id
-    remove_column :users, :facebook_app_secret
+    remove_column :partners, :facebook_link, :string
+    remove_column :users, :facebook_app_id, :text
+    remove_column :users, :facebook_app_secret, :text
   end
 end
