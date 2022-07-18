0fb10e49b628b4230fd7c533348b684be6a8f37c
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-07-18 17:13:37 +0100

    Filter for partners with no service areas
    Refactored get_map_markers to be more readable and it also supports
    Events directly.
    
    Tests covering get_map_markers helper method

--- app/controllers/concerns/map_markers.rb
+++ app/controllers/concerns/map_markers.rb
@@ -1,25 +1,58 @@
 module MapMarkers
+  extend ActiveSupport::Concern
 
-  # Takes an array of Partners and/or Addresses and returns a sanitized json
+  # Takes an array of Partners, Addresses or Events and returns a sanitized json
   # array suitable for creating map markers. Does not check for duplicates.
-  def get_map_markers(locations)
-    locations.reduce([]) do |arr, loc|
-      marker =
-        if (Partner == loc.class) && (loc&.address&.latitude)
-          {
-            lat: loc.address.latitude,
-            lon: loc.address.longitude,
-            name: loc.name,
-            id: loc.id
-          }
-        elsif loc.class == Address
-          {
-            lat: loc.latitude,
-            lon: loc.longitude
-          }
-        end
-      if marker then arr << marker else arr end
+  #
+  # parameters:
+  #   @locations - Array of Partners, Addresses or Events
+  #   @addresses_only - boolean whether to skip partners that
+  #     have no service areas
+  #
+  # returns:
+  #   Array of Hashes to be consumed by the JS map front end
+  #
+  def get_map_markers(locations, addresses_only=false)
+
+    # Events
+    locations = locations.map do |loc|
+      next loc unless loc.is_a?(Event)
+
+      loc.place || loc.address
     end
+
+    # Partners
+    locations = locations.map do |loc|
+      next loc unless loc.is_a?(Partner)
+
+      # reject partner with no resolvable address
+      next if loc.address&.latitude&.blank?
+
+      if addresses_only
+        # reject partner if they have no service areas?
+        next if loc.service_areas.count == 0
+      end
+
+      {
+        lat: loc.address.latitude,
+        lon: loc.address.longitude,
+        name: loc.name,
+        id: loc.id
+      }
+    end
+
+    # Addresses
+    locations = locations.map do |loc|
+      next loc unless loc.is_a?(Address)
+      {
+        lat: loc.latitude,
+        lon: loc.longitude
+      }
+    end
+
+    locations.keep_if { |loc|
+      # puts loc.class
+      loc.is_a?(Hash) }
   end
 
   # Takes a reducible collection of events and returns json map markers.
