cafd9fa55550dcc64948cf66730509bd012eb259
aaaaargZombies <ahberate@riseup.net>
2022-07-19 15:46:47 +0100

    add stimulus controller to neighbourhoods/_form.html.erb delete behaviors.neighbourhoods.js and remove from imports

--- app/javascript/src/behaviors/all_behaviors.js
+++ app/javascript/src/behaviors/all_behaviors.js
@@ -1,6 +1,5 @@
 import { Behaviors } from './behaviors.base.js'
 import './behaviors.collection.js'
-import './behaviors.neighbourhood.js'
 import './behaviors.partner.js'
 import './behaviors.place.js'
 import './behaviors.site.js'
