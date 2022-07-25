31b48a32380f553957bf05c6530038d9b3b4e54a
aaaaargZombies <ahberate@riseup.net>
2022-07-25 20:38:16 +0100

    add a test for users page

--- test/system/select2_inputs_test.rb
+++ test/system/select2_inputs_test.rb
@@ -25,6 +25,8 @@ class Select2InputTest < ApplicationSystemTestCase
     @calendar = create(:calendar, partner: @partner, place: @partner)
     @address = create :address
     create :event, address: @address, calendar: @calendar
+    create :tag
+    create :tag_public
 
     # logging in as root user
     visit '/users/sign_in'
@@ -34,56 +36,106 @@ class Select2InputTest < ApplicationSystemTestCase
   end
 
   test 'visiting a calendars form' do
-    # save_and_open_screenshot
-
-    within '.sidebar-sticky' do
-      link = page.find(:css, 'a[href*="calendars"]')
-      # puts link["href"]
-      # this works but why does the calendar exist but not be clickable?
-      visit link['href']
-      #
-      # click_link 'Calendars'
-      # looks like this redirects to "calendars/new" because there is no Calendar
-      # maybe this won't matter!? I don't think any of the inputs take calendars
-      #
-      # For some reason we have a calendar but theres some strange error in the
-      # events part of the table and you can't click through to edit it.
-    end
-
-    # save_and_open_screenshot
+    click_sidebar 'calendars'
     click_link 'Add New Calendar'
+
+    # test that select2 has rendered and is single
     partner_orginiser = page.all(:css, '.select2-container')[0]
     default_location = page.all(:css, '.select2-container')[1]
-    # save_and_open_screenshot
-    # select2_open from: 'Partner Organiser'
-    # select2 'Community Group 1', xpath: partner_orginiser.path
-    # select2 'Community Group 2', xpath: partner_orginiser.path
-    # within :xpath, partner_orginiser.path do
-    #   assert_selector '.select2-selection__rendered', text: 'Community Group 2'
-    # end
-    assert_select2_single partner_orginiser, 'Community Group 2'
-    assert_select2_single default_location, 'Community Group 3'
 
-    # select2s = page.all(:css, '.select2-container')
-    # select2 'Community Group 1', xpath: default_location.path
-    # select2 'Community Group 3', xpath: default_location.path
-    # within :xpath, default_location.path do
-    #   assert_selector '.select2-selection__rendered', text: 'Community Group 3'
-    # end
-    # select2-selection__rendered
+    select2 'Community Group 2', xpath: partner_orginiser.path
+    assert_select2_single 'Community Group 2', partner_orginiser
 
-    # select2_open css: '#calendar_place_id' # error
-    # Error:
-    # Select2InputTest#test_visiting_a_calendars_form:
-    # Capybara::Ambiguous: Ambiguous match, found 2 elements matching visible css "label:not(.select2-offscreen)" with text "Default location"
-    #   test/system/select2_inputs_test.rb:57:in `block in <class:Select2InputTest>'
+    select2 'Community Group 3', xpath: default_location.path
+    assert_select2_single 'Community Group 3', default_location
 
+    # create a new Calendar
+    fill_in 'Name', with: 'test cal'
+    fill_in 'URL', with: 'http://test.com/events.ics'
+    click_button 'Create Calendar'
+
+    # open edit form for new calendar to see if select2 has rendered and is displaying correct data
+    click_sidebar 'calendars'
+    await_datatables
+    click_link 'test cal'
+    partner_orginiser = page.all(:css, '.select2-container')[0]
+    default_location = page.all(:css, '.select2-container')[1]
+    assert_select2_single 'Community Group 2', partner_orginiser
+    assert_select2_single 'Community Group 3', default_location
+  end
+
+  test 'visiting a users form' do
+    click_sidebar 'users'
+    await_datatables
+
+    # edit a root user because they have access to all potential select2 inputs
+    datatable_1st_row = page.all(:css, '.odd')[0]
+    within datatable_1st_row do
+      click_link('Place')
+    end
+    partners = all_select2[0]
+    neighborhoods = all_select2[1]
+    tags = all_select2[2]
+    select2 'Community Group 1', 'Community Group 2', xpath: partners.path
+    assert_select2_multiple ['Community Group 1', 'Community Group 2'], partners
+    select2 'Ashton Hurst (Ward)', 'Ashton Hurst, Tameside (Ward)', xpath: neighborhoods.path
+    assert_select2_multiple ['Ashton Hurst (Ward)', 'Ashton Hurst, Tameside (Ward)'], neighborhoods
+    select2 'Hulme 1', 'Hulme 2', xpath: tags.path
+    assert_select2_multiple ['Hulme 1', 'Hulme 2'], tags
+    click_button 'Update'
+
+    click_sidebar 'users'
+    await_datatables
+
+    # return to user to check data is intact
+    datatable_1st_row = page.all(:css, '.odd')[0]
+    within datatable_1st_row do
+      click_link('Place')
+    end
+    partners = all_select2[0]
+    neighborhoods = all_select2[1]
+    tags = all_select2[2]
+    assert_select2_multiple ['Community Group 1', 'Community Group 2'], partners
+    assert_select2_multiple ['Ashton Hurst (Ward)', 'Ashton Hurst, Tameside (Ward)'], neighborhoods
+    assert_select2_multiple ['Hulme 1', 'Hulme 2'], tags
     save_and_open_screenshot
   end
-  def assert_select2_single(node, option)
-    select2 option, xpath: node.path
+
+  # I think the icons are interfering with click_link
+  def click_sidebar(href)
+    within '.sidebar-sticky' do
+      link = page.find(:css, "a[href*='#{href}']")
+      visit link['href']
+    end
+  end
+
+  def await_datatables(time = 15)
+    page.find(:css, '#datatable_info', wait: time)
+  end
+
+  def all_select2
+    page.all(:css, '.select2-container')
+  end
+
+  def assert_select2_single(option, node)
     within :xpath, node.path do
       assert_selector '.select2-selection__rendered', text: option
+    end
+  end
+
+  def assert_select2_multiple(options_array, node)
+    # The data is stored like this.
+    # "×Computer Access\n×Free WiFi\n×GM Systems Changers"
+    # The order is unpredictable so we can't build version from our options to test against
+    # instead copy the data, then pull out the options and joining characters
+    # If we are left with nothing then the options and stored data match
+    within :xpath, node.path do
+      assert_selector '.select2-selection__choice', count: options_array.length
+      rendered = find(:css, '.select2-selection__rendered').text.gsub('×', '').gsub("\n", '')
+      options_array.each do |opt|
+        rendered = rendered.gsub(opt, '')
+      end
+      assert_equal('', rendered, "'#{rendered}' is in the selected data but not in the options passed to this test")
     end
   end
 end
