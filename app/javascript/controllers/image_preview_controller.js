a07956fc03addba4260de77eb8f39e6ee2b4cc03
aaaaargZombies <ahberate@riseup.net>
2022-08-02 13:07:18 +0100

    add stimulus controller to handle image preview on partners form

--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -1,7 +1,19 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="image-preview"
 export default class extends Controller {
-  connect() {
-  }
+	static targets = ["img"];
+
+	connect() {}
+
+	file(event) {
+		const file = event.target.files[0];
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			const base64 = e.target.result;
+			this.imgTarget.src = base64;
+			this.imgTarget.style.display = "block";
+		};
+		reader.readAsDataURL(file);
+	}
 }
