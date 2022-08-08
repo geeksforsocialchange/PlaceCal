9e0b79f901e93fc83e360fd47a2bd921d70311bc
aaaaargZombies <ahberate@riseup.net>
2022-08-08 14:51:28 +0100

    add warning about passing args to leaflet controller

--- app/javascript/controllers/leaflet_controller.js
+++ app/javascript/controllers/leaflet_controller.js
@@ -1,6 +1,9 @@
 import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="leaflet"
+// Its important to use single quotes in the template when declaring the args values
+// data-leaflet-args-value='<%= args_for_map(points, site, local_assigns[:style], local_assigns[:compact]) %>'
+// If not you will get strange unicode values which will break parsing
 export default class extends Controller {
 	static values = { args: Object };
 	// {center, iconUrl, markers, shadowUrl, styleClass, tilesetUrl, zoom}
