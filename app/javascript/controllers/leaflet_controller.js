15cbe81d8e1ec0fb81b2358ae03e4a70b78066e9
aaaaargZombies <ahberate@riseup.net>
2022-08-08 17:17:57 +0100

    add a disconnect method to remove the map fix test for new selector

--- app/javascript/controllers/leaflet_controller.js
+++ app/javascript/controllers/leaflet_controller.js
@@ -8,21 +8,33 @@ import "leaflet";
 export default class extends Controller {
 	static values = { args: Object };
 	// {center, iconUrl, markers, shadowUrl, styleClass, tilesetUrl, zoom}
+
+	map;
+
 	connect() {
+		this.element.classList.add("map");
 		this.createMap();
 	}
 
+	disconnect() {
+		this.element.classList.remove("map");
+		this.argsValue.styleClass.forEach((className) => {
+			this.element.classList.remove(className);
+		});
+		this.map.remove();
+	}
+
 	createMap() {
-		const map = L.map(this.element);
+		this.map = L.map(this.element);
 		this.argsValue.styleClass.forEach((className) => {
 			this.element.classList.add(className);
 		});
-		map.scrollWheelZoom.disable();
+		this.map.scrollWheelZoom.disable();
 		L.tileLayer(this.argsValue.tilesetUrl, {
 			attribution: "PlaceCal",
 			maxZoom: 18,
-		}).addTo(map);
-		map.invalidateSize(true);
+		}).addTo(this.map);
+		this.map.invalidateSize(true);
 
 		const mapIcon = L.icon({
 			iconUrl: this.argsValue.iconUrl,
@@ -35,16 +47,18 @@ export default class extends Controller {
 			popupAnchor: [0, 0], // point from which the popup should open relative to the iconAnchor
 		});
 
-		map.setView(this.argsValue.center, this.argsValue.zoom);
+		this.map.setView(this.argsValue.center, this.argsValue.zoom);
 
 		const markers = this.argsValue.markers.map((m) => {
 			const mapMarker = L.marker(m.position, { icon: mapIcon });
-			mapMarker.addTo(map);
+			mapMarker.addTo(this.map);
 			mapMarker.bindPopup(m.anchor, { permanent: true, closeButton: false });
 			return mapMarker;
 		});
 
 		const markerGroup = L.featureGroup(markers);
-		map.fitBounds(markerGroup.getBounds(), { maxZoom: this.argsValue.zoom });
+		this.map.fitBounds(markerGroup.getBounds(), {
+			maxZoom: this.argsValue.zoom,
+		});
 	}
 }
