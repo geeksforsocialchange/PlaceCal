db9894407c098e7367b938c3a5da75ebe39ef682
aaaaargZombies <ahberate@riseup.net>
2022-08-09 12:53:15 +0100

    Remove unused behaviours JS and asociated DOM triggers (#1431)
    * remove behaviors import from admin
    
    * delete behaviors js folder
    
    * remove init_behaviors invocation from admin.js
    
    * remove behavior-triggers from DOM

--- app/javascript/admin.js
+++ app/javascript/admin.js
@@ -17,14 +17,10 @@ import "vue-turbolinks"
 import "./src/opening-times.js"
 
 // Specific pages
-import "./src/behaviors/all_behaviors.js"
 import "./src/calendar-form.js"
 import "./src/ward-picker.js"
 
 $(document).on("turbo:load", function () {
-
-  $("body").init_behaviors()
-
   $("[data-toggle='tooltip']").tooltip()
 });
 
