8ba5793b392e0392710ad869c12f4cbdb79c4838
aaaaargZombies <ahberate@riseup.net>
2022-08-09 16:14:59 +0100

    add some lifecycle stuff and make data visible

--- app/javascript/controllers/opening_times_controller.js
+++ app/javascript/controllers/opening_times_controller.js
@@ -1,9 +1,15 @@
 import { Controller } from "@hotwired/stimulus";
 
+const el = (type, content) => {
+	const el = document.createElement(type);
+	el.innerHTML = content;
+	return el;
+};
+
 // Connects to data-controller="opening-times"
 export default class extends Controller {
 	static values = { data: Array };
-	static targets = ["textarea"];
+	static targets = ["textarea", "list"];
 	connect() {
 		console.log("CONNECTED");
 		console.log(this.hasDataValue);
@@ -12,10 +18,20 @@ export default class extends Controller {
 
 	dataValueChanged() {
 		this.updateTextarea();
+		this.updateList();
 	}
 
 	updateTextarea() {
 		console.log("running");
 		this.textareaTarget.value = JSON.stringify(this.dataValue);
+	}
+
+	updateList() {
+		console.log("walking");
+		this.dataValue
+			.map((timeObj) => el("li", JSON.stringify(timeObj)))
+			.forEach((element) => {
+				this.listTarget.append(element);
+			});
 	}
 }
