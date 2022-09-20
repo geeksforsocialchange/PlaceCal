635fa94d5366085e48408ae533633b175efe6c93
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-09-20 14:31:21 +0100

    Docs: better description comment in system test

--- test/system/admin/partner_test.rb
+++ test/system/admin/partner_test.rb
@@ -100,8 +100,10 @@ class AdminPartnerTest < ApplicationSystemTestCase
     click_link @partner.name
     await_select2
 
-    # very specific bug here: if opening times has malformed data
-    #   it will crash the partner tags selector
+    # very specific bug in the view template here: if opening times has malformed data
+    #   it will cause problems for the javascript that runs the partner tags selector
+    #   (in the browser). so we verify the select2 code has worked by seeing if it has
+    #   correctly done its thing to the tag selector
     assert_selector '.partner_tags ul.select2-selection__rendered'
   end
 end
