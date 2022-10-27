ae5cd2a81212ae6d93c1b02ffbed5d178b977525
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-10-27 12:21:27 +0100

    Fix autoloading problem

--- app/jobs/calendar_importer/events/out_savvy_event.rb
+++ app/jobs/calendar_importer/events/out_savvy_event.rb
@@ -1,62 +1,60 @@
 # frozen_string_literal: true
 
-module CalendarImporter
-  module Events
-    class OutSavvyEvent < Base
-      attr_reader :full_type, :uid, :summary, :description, :dtstart, :dtend, :location
+module CalendarImporter::Events
+  class OutSavvyEvent < Base
+    attr_reader :full_type, :uid, :summary, :description, :dtstart, :dtend, :location
 
-      def self.extract_location(value)
-        location = value.first
-        return if location.blank?
+    def self.extract_location(value)
+      location = value.first
+      return if location.blank?
 
-        address = location['http://schema.org/address']&.first
-        return if address.blank?
+      address = location['http://schema.org/address']&.first
+      return if address.blank?
 
-        street_address = address['http://schema.org/streetAddress']
-        return if street_address.blank?
+      street_address = address['http://schema.org/streetAddress']
+      return if street_address.blank?
 
-        street_address&.first&.fetch('@value')
-      end
+      street_address&.first&.fetch('@value')
+    end
 
-      def initialize(event_hash)
-        super event_hash
+    def initialize(event_hash)
+      super event_hash
 
-        event_hash.each do |key, value|
-          case key
-          when '@type'
-            @full_type = value.first
-          when 'http://schema.org/description'
-            @description = value.first['@value']
-          when 'http://schema.org/endDate'
-            @dtend = value.first['@value']
-          when 'http://schema.org/startDate'
-            @dtstart = value.first['@value']
-          when 'http://schema.org/name'
-            @summary = value.first['@value']
-          when 'http://schema.org/url'
-            @url = value.first['@id']
-            @uid = @url
-          when 'http://schema.org/location'
-            @location = OutSavvyEvent.extract_location(value)
-          end
+      event_hash.each do |key, value|
+        case key
+        when '@type'
+          @full_type = value.first
+        when 'http://schema.org/description'
+          @description = value.first['@value']
+        when 'http://schema.org/endDate'
+          @dtend = value.first['@value']
+        when 'http://schema.org/startDate'
+          @dtstart = value.first['@value']
+        when 'http://schema.org/name'
+          @summary = value.first['@value']
+        when 'http://schema.org/url'
+          @url = value.first['@id']
+          @uid = @url
+        when 'http://schema.org/location'
+          @location = OutSavvyEvent.extract_location(value)
         end
       end
+    end
 
-      def type
-        @type ||= @full_type.split('/').last
-      end
+    def type
+      @type ||= @full_type.split('/').last
+    end
 
-      def event_record?
-        type == 'Event' || type == 'VisualArtsEvent'
-      end
+    def event_record?
+      type == 'Event' || type == 'VisualArtsEvent'
+    end
 
-      def place
-        ''
-      end
+    def place
+      ''
+    end
 
-      def occurrences_between(*)
-        [Dates.new(dtstart, dtend)]
-      end
+    def occurrences_between(*)
+      [Dates.new(dtstart, dtend)]
     end
   end
 end
