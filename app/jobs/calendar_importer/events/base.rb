748d0a028386323beeb9170d712d2a8fd3e1c0a5
Ivan Kocienski <ivan.kocienski@gmail.com>
2022-11-07 17:35:54 +0000

    Fix: cleaning up HTML before parsing into markdown
    Strip HTML tags of their attributes (but keep <a> tag hrefs)

--- app/jobs/calendar_importer/events/base.rb
+++ app/jobs/calendar_importer/events/base.rb
@@ -31,9 +31,11 @@ module CalendarImporter::Events
       input = input.to_s.strip
       return '' if input.blank?
 
+      # attempt to get rid of broken UTF-8
       clean_text = sanitize_invalid_char(input)
       input_mode = 'markdown'
 
+      # do we have HTML? yeah let's get rid of that
       doc = Nokogiri::HTML.fragment(clean_text)
       if doc.css('*').length.positive?
         input_mode = 'html'
@@ -47,6 +49,15 @@ module CalendarImporter::Events
 
         doc.css('h1', 'h2').each { |header| header.name = 'h3' }
 
+        # if we get HTML then remove all the attributes from all of
+        # the tags so it doesn't interfere with the kramdown step
+        doc.css('*').each do |tag|
+          tag.keys.each do |attribute_name|
+            next if tag.name == 'a' && attribute_name == 'href'
+            tag.remove_attribute attribute_name
+          end
+        end
+
         if footer.present?
           doc << '<br/><br/>'
           doc << footer
@@ -56,6 +67,7 @@ module CalendarImporter::Events
         clean_text = ActionController::Base.helpers.sanitize(body_text, tags: ALLOWED_TAGS)
       end
 
+      # convert HTML tags into markdown kramdown
       Kramdown::Document.new(clean_text, input: input_mode).to_kramdown.strip
     end
 
