80a6461d2b7a732d781871591107623e3f866d39
Ivan Kocienski <ivan.kocienski@gmail.com>
2023-02-07 13:50:11 +0000

    Calendar Importer Eventbrite parser Bad gateway fix

--- test/jobs/calendar_importer/eventbrite_parser_test.rb
+++ test/jobs/calendar_importer/eventbrite_parser_test.rb
@@ -23,4 +23,29 @@ class EventbriteParserTest < ActiveSupport::TestCase
       assert_equal 17, records.count
     end
   end
+
+  test 'ignores 504 bad gateway responses' do
+    os_event_url = 'https://www.eventbrite.co.uk/o/ftm-london-32888898939'
+
+    VCR.use_cassette(:eventbrite_bad_gateway) do
+      calendar = build(
+        :calendar,
+        strategy: :event,
+        name: :import_test_calendar,
+        source: os_event_url
+      )
+
+      parser = CalendarImporter::Parsers::Eventbrite.new(calendar, url: os_event_url)
+
+      begin
+        records = parser.download_calendar
+      rescue RestClient::BadGateway
+        # this is essentially validating that the download_calendar
+        #    method is NOT raising a BadGateway exception.
+        #    There is a bug in how RestClient reports the BadGateway
+        #    exception is raised that itself was causing an exception.
+        flunk 'Was not expecting RestClient::BadGateway'
+      end
+    end
+  end
 end
