fb36c3ec63f269667ee19296f7c3bd836af85e24
aaaaargZombies <ahberate@riseup.net>
2023-03-23 13:57:20 +0000

    fixes #1763 limit tags shown to root user if they are also a (Partnership)tag_admin (#1764)

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -30,7 +30,7 @@ class Tag < ApplicationRecord
   validate :check_editable_fields
 
   scope :users_tags, lambda { |user|
-                       return Tag.all if user.role == 'root'
+                       return Tag.all if user.role == 'root' && !user.tag_admin?
 
                        partnership_tags = Tag.where(type: 'Partnership', id: user.tags_users.distinct.pluck(:tag_id)).pluck(:id)
                        other_tags = Tag.where("type != 'Partnership'").pluck(:id)
