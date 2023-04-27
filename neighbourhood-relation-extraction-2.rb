a8ff9a92e529ae70ce9614bbab6978be1c96ea08
Ivan Kocienski <ivan.kocienski@gmail.com>
2023-04-27 15:55:11 +0100

    Capture of work done so far

--- neighbourhood-relation-extraction-2.rb
+++ neighbourhood-relation-extraction-2.rb
@@ -9,10 +9,20 @@ module Extractor
   def run
     payload = {}
 
+    payload[:timestamp] = Time.now.to_s
+    puts "running on #{payload[:timestamp]}"
+    
     payload[:users] = process_users
+    puts "users.count=#{payload[:users].count}"
+
     payload[:service_area_partners] = process_partners
+    puts "service_area_partners.count=#{payload[:service_area_partners].count}"
+
     payload[:addresses] = process_addresses
+    puts "addresses.count=#{payload[:addresses].count}"
+
     payload[:sites] = process_sites
+    puts "sites.count=#{payload[:sites].count}"
 
     File.open('neighbourhood-relation-info.json', 'w') do |file|
       file.puts payload.to_json
@@ -23,7 +33,7 @@ module Extractor
     found = {}
     
     User.all.each do |user|
-      user[user.id] = user.neighbourhoods.pluck(:unit_code_value)
+      found[user.id] = user.neighbourhoods.pluck(:unit_code_value)
     end
 
     found
@@ -33,7 +43,7 @@ module Extractor
     found = {}
 
     Partner.all.each do |partner|
-      found[partner.id] = partner.service_areas.pluck(:unit_code_value)
+      found[partner.id] = partner.service_area_neighbourhoods.pluck(:unit_code_value)
     end
     
     found
@@ -43,7 +53,10 @@ module Extractor
     found = {}
     
     Address.all.each do |address|
-      found[address.id] = address.neighbourhood.unit_code_value
+      hood = address.neighbourhood
+      next if hood.nil?
+      
+      found[address.id] = hood.unit_code_value
     end
     
     found
@@ -53,7 +66,10 @@ module Extractor
     found = {}
 
     Site.all.each do |site|
-      found[site.id] = site.neighbourhoods.pluck(:unit_code_value)
+      found[site.id] = {
+        primary: site.primary_neighbourhood&.unit_code_value,
+        secondary: site.secondary_neighbourhoods.pluck(:unit_code_value)
+      }
     end
     
     found
@@ -61,3 +77,24 @@ module Extractor
 end
 
 Extractor.run
+
+=begin
+given an import job where data must be reconciled between data
+that exists in the DB and data present in the snapshot files,
+key off of data in the DB.
+
+so this
+Partner.each do |partner|
+
+service_areas = import_data[:partners][partner.id]
+
+service_areas.each do |unit_code|
+neighbourhood = Neighbourhood.where(unit_code: unit_code).first
+if neighbourhood.nil?
+we are mising a neighbourhood for partner ID
+next
+end
+end
+end
+
+=end
