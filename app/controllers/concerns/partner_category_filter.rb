109fae421fc6b84d9bdcb328ebc6a5c7e0b39f41
ivan <ivan@localhost.localdomain>
2023-05-01 13:19:23 +0100

    Partner tag filter refactor

---
+++ app/controllers/concerns/partner_category_filter.rb
@@ -0,0 +1,134 @@
+class PartnerCategoryFilter
+  def initialize(params)
+    @current_category_id = params[:category]
+    @current_category = Category.where(id: @current_category_id).first if @current_category_id.present?
+    
+    @include_mode = params[:mode] # include/exclude
+  end
+
+  def is_active?
+    @current_category.present?
+  end
+  
+  def apply_to(query=Partner)
+    return query if !is_active?
+
+    if exclude_mode?
+      return query
+               .left_joins(:partner_tags)
+               .where("(partner_tags.id IS NULL) OR (partner_tags.tag_id != ?)", @current_category.id)
+    end
+    
+    query
+      .joins(:partner_tags)
+      .where("partner_tags.tag_id = ?", @current_category.id)
+   end
+  
+  def categories
+    @categories ||= Category
+                      .joins(:partner_tags)
+                      .group('tags.id')
+                      .having('count(tag_id) > 0')
+                      .order(:name)
+  end
+  
+  def show_filter?
+    categories.present?
+  end
+
+  def current_category?(category)
+    # puts "current_category=#{@current_category}"
+    # puts "category=#{category}"
+    @current_category.present? && (@current_category.id == category.id)
+  end
+  
+  def render_filter(view)
+    view.render partial: 'partners/category_filter', locals: { filter: self }
+  end
+
+  def include_mode?
+    !exclude_mode?
+  end
+
+  def exclude_mode?
+    @include_mode == 'exclude'
+  end
+end
+
+=begin
+<% # this is the front end component of app/controllers/concerns/partner_category_filter.rb
+   %>
+
+<div class="breadcrumb__filters filters">
+  <div class="js-filters-toggle filters__toggle">
+    <a href="#" data-turbo="false"><span class="icon icon--arrow-down">↓</span> <span class="filters__link">Category</span></a>
+  </div>
+  
+  <div class="filters__dropdown filters__dropdown--hidden js-filters-dropdown">
+    <%= form_tag partners_path, method: :get, class: 'filters__form' do %>
+    <div class="filters__group" >
+      <% categories.each do |category| %>
+      <div class="filters__option">
+        <%= radio_button_tag 'category', category.id, filter.current_category?(category), class: "tag__button" %>
+        <%= label_tag 'category', category.name, class: "tag__label" %>
+      </div>
+      <% end %>
+    </div>
+    
+    <hr>
+    
+    <div class="filters__group">
+      <div class="filters__option">
+        <%= radio_button_tag 'mode', 'include', false, class: "include__button" %>
+        <%= label_tag 'mode', 'Include', class: "include__label" %>
+      </div>
+      <div class="filters__option">
+        <%= radio_button_tag 'mode', 'exclude', false, class: "include__button" %>
+        <%= label_tag 'mode', 'Exclude', class: "include__label" %>
+      </div>
+    </div>
+    
+    <hr>
+
+    <a href="/partners" class="btn">Reset</a>
+    <%= button_tag 'Filter', name: nil, class: 'btn' %>
+    <% end %>
+  </div>
+</div>
+
+
+
+
+
+
+
+  def tag
+    @current_tag = params[:opts][:id]
+    @include = params[:opts][:include] == '1'
+    site_partners = Partner
+                    .for_site(current_site)
+
+    tag_partners = Tag.find(@current_tag).partners.pluck(:id)
+
+    @partners = if @current_tag
+                  if @include
+                    Partner.where(id: site_partners.pluck(:id) & tag_partners)
+                           .includes(:service_areas, :address)
+                           .order(:name)
+                  else
+                    Partner.where(id: site_partners.pluck(:id) - tag_partners)
+                           .includes(:service_areas, :address)
+                           .order(:name)
+                  end
+                else
+                  site_partners.order(:name)
+                end
+
+    # show only partners with no service_areas
+    @map = get_map_markers(@partners, true) if @partners.detect(&:address)
+    # cat_tags = Tag.where(type: 'Category').order(:name)
+    # @categories = cat_tags.filter { |t| (@partners & t.partners).length.positive? }
+    # render 'index'
+    # render 'tag'
+  end
+=end
---
+++ test/controllers/concerns/partner_category_filter_test.rb
@@ -0,0 +1,105 @@
+
+require 'test_helper'
+
+class PartnerCategoryFilterTest < ActionDispatch::IntegrationTest
+  setup do
+    @category = create(:category)
+    @categories = create_list(:category, 4)
+    @categories << @category
+  end
+
+  test '#is_active?' do
+    # is false by default
+    filter = PartnerCategoryFilter.new({})
+    assert filter.is_active? == false, 'is_active? should be false with no input'
+    filter = PartnerCategoryFilter.new(category: @category.id)
+    assert filter.is_active?, 'should be active'
+  end
+
+  test '#categories' do
+    # doesn't find categories that lack partners
+    filter = PartnerCategoryFilter.new({})
+    found = filter.categories
+    assert found.length == 0
+
+    # does find categories assigned to partners
+    given_some_partnered_categories_exist
+    
+    filter = PartnerCategoryFilter.new({})
+    found = filter.categories
+    assert found.length == 5
+  end
+
+  test '#apply_to' do
+    partner_1 = create(:partner)
+    partner_2 = create(:partner)
+    partner_3 = create(:partner)
+    partner_4 = create(:partner)
+    
+    # returns every partner if no filter category set
+    filter = PartnerCategoryFilter.new({})
+    query = filter.apply_to(Partner)
+    assert query.count == 4
+
+    # returns only partners assigned category (in include mode)
+    partner_1.tags << @category
+    
+    filter = PartnerCategoryFilter.new(category: @category.id)
+    query = filter.apply_to(Partner)
+    assert query.count == 1
+
+    # returns only partners NOT assigned category (in exclude mode)
+    filter = PartnerCategoryFilter.new(category: @category.id, mode: 'exclude')
+    query = filter.apply_to(Partner)
+    assert query.count == 3
+  end
+
+  test '#show_filter?' do
+    # is false if no partnered categories found
+    filter = PartnerCategoryFilter.new({})
+    assert filter.show_filter? == false
+    
+    # is true when partnered categories found
+    given_some_partnered_categories_exist
+    
+    filter = PartnerCategoryFilter.new({})
+    assert filter.show_filter?
+  end
+
+  test '#current_category?' do
+    # is false when no current category set
+    filter = PartnerCategoryFilter.new({})
+    assert filter.current_category?(@category) == false
+
+    # is true when current category matches incoming category
+    filter = PartnerCategoryFilter.new(category: @category.id)
+    assert filter.current_category?(@category) == true
+  end
+
+  test 'modes' do
+    # default to include mode
+    filter = PartnerCategoryFilter.new({})
+    assert filter.include_mode?
+    assert_not filter.exclude_mode?
+    
+    # is include mode when set
+    filter = PartnerCategoryFilter.new(mode: 'include')
+    assert filter.include_mode?
+    assert_not filter.exclude_mode?
+    
+    # is exclude mode when set
+    filter = PartnerCategoryFilter.new(mode: 'exclude')
+    assert filter.exclude_mode?
+    assert_not filter.include_mode?
+  end
+
+  # helpers
+  def given_some_partnered_categories_exist
+    partner = create(:partner)
+    
+    @categories.each do |category|
+      partner.tags << category      
+    end
+  end
+end
+
