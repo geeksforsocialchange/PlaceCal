2e4380662d79260451b32a6503d76632b8f0702d
Rosemary Ferrier <rosemary.ferrier@gmail.com>
2023-05-11 16:50:52 +0100

    add new component for case studies & move pull quote to component

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 //= link_tree ../fonts
 //= link_tree ../images
 //= link_tree ../pdfs
+//= link application.css
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets/themes .css
 //= link_directory ../stylesheets/themes/custom .css
---
+++ app/components/case_study_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
+class CaseStudyComponent < ViewComponent::Base
+  def initialize(partner:)
+    super
+    partner_info = YAML.load_file(
+      File.join(__dir__, 'case_study_component/case_study_data.yml')
+    )[partner]
+
+    @partner = partner_info['partner']
+    @logo_src = partner_info['logo_src']
+    @image_src = partner_info['image_src']
+    @image_alt = partner_info['image_alt']
+    @partner_url = partner_info['partner_url']
+    @description = partner_info['description']
+    @pull_quote = partner_info['pull_quote']
+  end
+end
