a3c542bec4ba4b670406b5455ee23268e45b2456
Ivan Kocienski <ivan.kocienski@gmail.com>
2023-05-22 19:24:25 +0100

    removed unused test argument

--- test/controllers/partners_controller_test.rb
+++ test/controllers/partners_controller_test.rb
@@ -17,8 +17,6 @@ class PartnersControllerTest < ActionDispatch::IntegrationTest
       a
     end
 
-    @site = create(:site)
-
     # NOTE: Uhhh? What? Is this a factorybot thing? Why does create(:address, neighbourhood: n) not work here?
 
     @partners = addresses.map do |a|
