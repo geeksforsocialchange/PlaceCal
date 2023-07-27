4bb8bfbf2c857709a8f2b7dfec97e66747981454
Rosemary Ferrier <rosemary.ferrier@gmail.com>
2023-07-27 12:25:19 +0100

    add back migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_24_125445) do
+ActiveRecord::Schema.define(version: 2023_07_27_111308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,6 +166,7 @@ ActiveRecord::Schema.define(version: 2023_07_24_125445) do
     t.string "unit_code_value"
     t.string "unit_name"
     t.string "parent_name"
+    t.datetime "release_date"
     t.index ["ancestry"], name: "index_neighbourhoods_on_ancestry"
   end
 
