1a8a8f767db83eb8008cd561f5afcd9da947eda7
Rosemary Ferrier <rosemary.ferrier@gmail.com>
2023-11-02 11:48:09 +0000

    fix edit bug and update tests

--- test/models/partner_address_or_service_area_presence_test.rb
+++ test/models/partner_address_or_service_area_presence_test.rb
@@ -4,7 +4,8 @@ require 'test_helper'
 
 class PartnerAddressOrServiceAreaPresenceTest < ActiveSupport::TestCase
   setup do
-    @user = create(:root)
+    @user = create(:neighbourhood_admin)
+    @root_user = create(:root)
     @neighbourhood = neighbourhoods(:one)
     @user.neighbourhoods << @neighbourhood
 
@@ -13,9 +14,16 @@ class PartnerAddressOrServiceAreaPresenceTest < ActiveSupport::TestCase
       summary: 'Summary of alpha',
       accessed_by_user: @user
     )
+    @new_partner_for_root = Partner.new(
+      name: 'Alpha name',
+      summary: 'Summary of alpha',
+      accessed_by_user: @root_user
+    )
   end
 
-  test 'is invalid if both service area and address not present' do
+  ####  neighbourhood admin tests
+
+  test 'with neighbourhood admin user - is invalid if both service area and address not present' do
     @new_partner.validate
 
     assert_not_predicate(@new_partner, :valid?, 'Partner should be invalid')
@@ -24,14 +32,34 @@ class PartnerAddressOrServiceAreaPresenceTest < ActiveSupport::TestCase
     assert_predicate base_errors.length, :positive?
   end
 
-  test 'is valid with service_area set' do
+  test 'with neighbourhood admin user - is invalid if service area set outside neighbourhood and address not present' do
+    @new_partner.service_areas.build neighbourhood: create(:neighbourhood)
+    @new_partner.validate
+
+    assert_not_predicate(@new_partner, :valid?, 'Partner should be invalid')
+
+    base_errors = @new_partner.errors[:base]
+    assert_predicate base_errors.length, :positive?
+  end
+
+  test 'with neighbourhood admin user - is invalid if address set outside neighbourhood and service area not present' do
+    address = build(:address, neighbourhood: create(:neighbourhood))
+    @new_partner.validate
+
+    assert_not_predicate(@new_partner, :valid?, 'Partner should be invalid')
+
+    base_errors = @new_partner.errors[:base]
+    assert_predicate base_errors.length, :positive?
+  end
+
+  test 'with neighbourhood admin user - is valid with owned service_area set' do
     @new_partner.service_areas.build neighbourhood: @neighbourhood
     @new_partner.validate
 
     assert_predicate(@new_partner, :valid?, 'Partner should be valid')
   end
 
-  test 'is valid with address set' do
+  test 'with neighbourhood admin user - is valid with owned address set' do
     address = build(:address, neighbourhood: @neighbourhood)
 
     @new_partner.address = address
@@ -40,13 +68,40 @@ class PartnerAddressOrServiceAreaPresenceTest < ActiveSupport::TestCase
     assert_predicate(@new_partner, :valid?, 'Partner should be valid')
   end
 
-  test 'is valid with both service_area and address set' do
+  ####  root user tests
+
+  test 'with root user - is invalid if both service area and address not present' do
+    @new_partner_for_root.validate
+
+    assert_not_predicate(@new_partner_for_root, :valid?, 'Partner should be invalid')
+
+    base_errors = @new_partner_for_root.errors[:base]
+    assert_predicate base_errors.length, :positive?
+  end
+
+  test 'with root user - is valid with service_area set' do
+    @new_partner_for_root.service_areas.build neighbourhood: create(:neighbourhood)
+    @new_partner_for_root.validate
+
+    assert_predicate(@new_partner_for_root, :valid?, 'Partner should be valid')
+  end
+
+  test 'with root user - is valid with address set' do
+    address = build(:address, neighbourhood: create(:neighbourhood))
+
+    @new_partner_for_root.address = address
+    @new_partner_for_root.save!
+
+    assert_predicate(@new_partner_for_root, :valid?, 'Partner should be valid')
+  end
+
+  test 'with root user - is valid with both service_area and address set' do
     address = build(:address)
 
-    @new_partner.address = address
-    @new_partner.service_areas.build neighbourhood: @neighbourhood
-    @new_partner.validate
+    @new_partner_for_root.address = address
+    @new_partner_for_root.service_areas.build neighbourhood: create(:neighbourhood)
+    @new_partner_for_root.validate
 
-    assert_predicate(@new_partner, :valid?, 'Partner should valid')
+    assert_predicate(@new_partner_for_root, :valid?, 'Partner should valid')
   end
 end
