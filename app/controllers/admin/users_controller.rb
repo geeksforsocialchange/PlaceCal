044e7f54217b70dab6b87bcd8b0c230afca207e7
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2023-11-03 14:07:22 +0000

    comment of shame

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -127,7 +127,8 @@ module Admin
     end
 
     def validate_neighbourhood_relation
-      # sorry this is a monstrosity, it's what robocop made me do
+      # sorry this is a monstrosity, it's what robocop made me do it
+      # we only allow the neighbourhood_admin to assign Partners from their own Neighbourhood so any ids here are proof of a relationship
       return unless current_user.neighbourhood_admin? && !current_user.root? && params[:user][:partner_ids].reject { |id| id == '' }.empty?
 
       # we're about to refresh the page so save the users input
