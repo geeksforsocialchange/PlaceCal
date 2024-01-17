4647a69b4b751b6219df685f8eaebbdf9931362a
Ivan Kocienski (GFSC) <109223824+ivan-kocienski-gfsc@users.noreply.github.com>
2024-01-17 12:10:57 +0000

    Revert "Rename badly named column in model"

--- app/models/organisation_relationship.rb
+++ app/models/organisation_relationship.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
 class OrganisationRelationship < ApplicationRecord
-  # NOTE: This model is (was) used to define a relationship between
-  #   partners so one "big" partner can have lots of "small" partners "within"
-  #   the code here is preserved to maintain active functionality on the production
-  #   site but please note there is no way for admins to operate on this data
-  #   directly.
-  #   we hope to define this relationship properly later on and remove this
-  #   confusing model but leaving as is for now -IK
+  self.table_name = 'organisation_relationships'
 
-  # rubocop:disable Rails/InverseOf
-  belongs_to :subject, class_name: 'Partner', foreign_key: 'partner_subject_id'
-  belongs_to :object, class_name: 'Partner', foreign_key: 'partner_object_id'
-  # rubocop:enable Rails/InverseOf
-
+  belongs_to :subject, class_name: 'Partner'
   extend Enumerize
   enumerize :verb, in: %i[manages]
+  belongs_to :object, class_name: 'Partner'
 end
--- db/migrate/20240116150528_rename_organisation_relation_object_column.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RenameOrganisationRelationObjectColumn < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :organisation_relationships, :object_id, :partner_object_id
-    rename_column :organisation_relationships, :subject_id, :partner_subject_id
-  end
-end
