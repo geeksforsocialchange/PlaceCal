3b1547d8d3fdeb714a25490282c0eb639d719fe6
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-02-19 16:14:48 +0000

    remove resolved comment

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -40,7 +40,6 @@ class PartnersController < ApplicationController
   # GET /partners/1
   # GET /partners/1.json
   def show
-    # possibly should say partner not found or something
     redirect_to root_path if @partner.hidden
 
     upcoming_events = Event.by_partner(@partner).upcoming
