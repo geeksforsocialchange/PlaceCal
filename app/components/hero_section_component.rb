8077c2f2db7e8fabf7d9379fa47ed02e06474fdd
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-03-20 14:13:06 +0000

    implement hero_section component

--- app/components/hero_section_component.rb
+++ app/components/hero_section_component.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class HeroSectionComponent < ViewComponent::Base
-  def initialize(title:, image_path:, image_credit:)
+  def initialize(image_path:, image_credit:, title:)
     super
-    @title = title
+    @title = title.empty? ? 'PlaceCal is a community events calendar where you can find everything near you, all in one place.' : title
     @image_path = image_path
     @image_credit = image_credit
   end
