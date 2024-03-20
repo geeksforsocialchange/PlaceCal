7fc8181d87832aaed95dc22d322a30feed2a7afb
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-03-20 14:27:02 +0000

    tidy up old code and tweak for tests / missing data

--- app/components/hero_section_component.rb
+++ app/components/hero_section_component.rb
@@ -3,7 +3,7 @@
 class HeroSectionComponent < ViewComponent::Base
   def initialize(image_path:, image_credit:, title:)
     super
-    @title = title.empty? ? 'PlaceCal is a community events calendar where you can find everything near you, all in one place.' : title
+    @title = title.presence || 'PlaceCal is a community events calendar where you can find everything near you, all in one place.'
     @image_path = image_path
     @image_credit = image_credit
   end
