320c3aa8a275de5853aebef85d943bacd54a5c1b
Ivan Kocienski <ivan@gfsc.studio>
2024-04-02 11:06:58 +0100

    Debouncing admin partner name check handler

--- app/javascript/controllers/partner_form_validation_controller.js
+++ app/javascript/controllers/partner_form_validation_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
+import _ from "lodash";
 
 // PartnerFormValidationController
 export default class extends Controller {
@@ -7,14 +8,14 @@ export default class extends Controller {
 	connect() {
 		console.log("PartnerFormValidationController.connect");
 
-		//this.inputDebouncePeriod = 200;
-		this.baseUrl = "/partners/lookup_name";
+		const inputDebouncePeriod = 500; // ms
+		const baseUrl = "/partners/lookup_name";
 
 		const csrfToken = document
 			.querySelector('meta[name="csrf-token"]')
 			.getAttribute("content");
 
-		this.endpointHeaders = {
+		const endpointHeaders = {
 			method: "GET",
 			headers: {
 				Accept: "application/json",
@@ -23,39 +24,41 @@ export default class extends Controller {
 			},
 		};
 
-		this.nameProblemFeedbackElement = document.getElementById(
+		const nameProblemFeedbackElement = document.getElementById(
 			"partner-name-feedback"
 		);
 
-		this.nameField = this.sourceTarget;
-		this.originalValue = this.nameField.value;
+		const nameField = this.sourceTarget;
+		const originalValue = nameField.value;
+
+		this.inputFunction = _.debounce(() => {
+			nameField.classList.remove("is-invalid");
+			nameProblemFeedbackElement.style.display = "none";
+
+			const nameValue = nameField.value;
+			if (nameValue.length < 5 || nameValue === originalValue) {
+				return;
+			}
+
+			const tryName = encodeURIComponent(nameValue);
+			const url = `${baseUrl}?name=${tryName}`;
+
+			fetch(url, endpointHeaders)
+				.then((response) => response.json())
+				.then((data) => {
+					if (!data.name_available) {
+						nameField.classList.add("is-invalid");
+						nameProblemFeedbackElement.style.display = "block";
+					}
+
+					nameField.focus();
+				});
+		}, inputDebouncePeriod);
 	}
 
 	disconnect() {}
 
 	checkInput(event) {
-		console.log("PartnerFormValidationController.checkInput");
-
-		this.nameField.classList.remove("is-invalid");
-		this.nameProblemFeedbackElement.style.display = "none";
-
-		const nameValue = this.sourceTarget.value;
-		if (nameValue.length < 5 || nameValue === this.originalValue) {
-			return;
-		}
-
-		const tryName = encodeURIComponent(nameValue);
-		const url = `${this.baseUrl}?name=${tryName}`;
-
-		fetch(url, this.endpointHeaders)
-			.then((response) => response.json())
-			.then((data) => {
-				if (!data.name_available) {
-					this.nameField.classList.add("is-invalid");
-					this.nameProblemFeedbackElement.style.display = "block";
-				}
-
-				this.nameField.focus();
-			});
+		this.inputFunction();
 	}
 }
