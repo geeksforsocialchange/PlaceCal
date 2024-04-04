db5aa77c0faf9e0d99fd97c5f5a452f61de04d90
Ivan Kocienski <ivan@gfsc.studio>
2024-04-04 10:47:29 +0100

    Quick fix for breaking system tests
    Makes the virtual window longer so maybe selenium can find the correct
    link to click on

--- test/system/application_system_test_case.rb
+++ test/system/application_system_test_case.rb
@@ -5,7 +5,7 @@ require 'selenium/webdriver'
 
 # For system tests so we can test our JS frontend is working
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 2400]
 
   Capybara.server = :puma, { Silent: true }
   Selenium::WebDriver.logger.ignore(:browser_options)
