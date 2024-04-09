3d2336661ce5fbd66a43ae5d663333931c9a70ee
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-04-09 15:47:42 +0100

    increment the window for showing events on the index page till we get atleast 10

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,10 +14,29 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    # Duration to view - default to day view
-    @period = params[:period].to_s || 'day'
     @repeating = params[:repeating] || 'on'
-    @events = filter_events(@period, repeating: @repeating, site: current_site)
+    # Duration to view - default to day view
+    if params[:period].to_s == ''
+      @period = 'day'
+      events = filter_events(@period, repeating: @repeating, site: current_site)
+      if events.count < 10
+        @period = 'week'
+        events = filter_events(@period, repeating: @repeating, site: current_site)
+        if events.count < 10
+          @period = 'future'
+          @events = filter_events(@period, repeating: @repeating, site: current_site)
+        else
+          @events = events
+        end
+      else
+        @events = events
+      end
+
+    else
+      @period = params[:period].to_s
+      @events = filter_events(@period, repeating: @repeating, site: current_site)
+    end
+
     @title = current_site.name
     # Sort criteria
     @events = sort_events(@events, @sort)
