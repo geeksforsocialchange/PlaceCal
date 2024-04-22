a5c0b4ed5871883370ec7d48bdc8ff7230442381
Ivan Kocienski <ivan@gfsc.studio>
2024-04-22 15:44:19 +0100

    Removed unhelpful comment

--- app/jobs/calendar_importer/events/ics_event.rb
+++ app/jobs/calendar_importer/events/ics_event.rb
@@ -59,10 +59,6 @@ module CalendarImporter::Events
 
       return if link.blank?
 
-      # Then grab the first element of either the match object or the conference array
-      # (The match object returns ICal Text, not a String, so we have to cast)
-      # (We can't use .first here because the match object doesn't support it!)
-      #
       online_address = OnlineAddress.find_or_create_by(url: link,
                                                        link_type: have_direct_url_to_stream?(link))
       online_address.id
