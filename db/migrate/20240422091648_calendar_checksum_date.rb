faea453e14b66a2682aa7f90263c643558a7b000
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-04-23 13:34:48 +0100

    make requested changes to PR

--- db/migrate/20240422091648_calendar_checksum_date.rb
+++ db/migrate/20240422091648_calendar_checksum_date.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CalendarChecksumDate < ActiveRecord::Migration[7.1]
-  def change
+  def up
     add_column :calendars, :checksum_updated_at, :datetime
   end
 end
