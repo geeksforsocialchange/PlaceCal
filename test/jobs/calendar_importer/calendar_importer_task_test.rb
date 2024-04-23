710e1747f14495fea50223f3acf9aa1e86fec72f
Rosemary Ferrier <rosemary.ferrier@gmail.com>
2024-04-23 09:59:55 +0100

    fix tests

--- test/jobs/calendar_importer/calendar_importer_task_test.rb
+++ test/jobs/calendar_importer/calendar_importer_task_test.rb
@@ -87,7 +87,7 @@ class CalendarImporterTaskTest < ActiveSupport::TestCase
       assert_equal 'eventbrite', calendar.importer_used
 
       created_events = calendar.events
-      assert_equal 1, created_events.count
+      assert_equal 17, created_events.count
     end
   end
 
