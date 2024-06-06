57ef8d032b3a5cfb3aa838b9dd32d419f5219ce2
Dr Kim Foale <kim@gfsc.studio>
2024-06-06 17:11:34 +0100

    fix: move hard coded string to locale file

--- app/components/hero_section_component.rb
+++ app/components/hero_section_component.rb
@@ -3,8 +3,7 @@
 class HeroSectionComponent < ViewComponent::Base
   def initialize(image_path:, image_credit:, title:, alttext:)
     super
-    # TODO: remove hard coded string from component
-    @title = title.presence || "PlaceCal is a community events calendar where you can find out everything that's happening, all in one place."
+    @title = title.presence || I18n.t('meta.description', site: 'PlaceCal')
     @image_path = image_path
     @alttext = alttext
     @image_credit = image_credit
