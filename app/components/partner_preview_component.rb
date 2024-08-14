10fe8b93e79303eff93f63f554fa11306470634e
Dr Kim Foale <kim@gfsc.studio>
2024-08-14 21:21:15 +0100

    chore: Refactor partner preview component

---
+++ app/components/partner_preview_component.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class PartnerPreviewComponent < ViewComponent::Base
+  def initialize(partner:, site:)
+    super
+    @partner = partner
+    @site = site
+    @show_neighbourhoods = @site.show_neighbourhoods?
+    @neighbourhood_name = @partner.neighbourhood_name_for_site(@site.badge_zoom_level)
+  end
+
+  def show_neighbourhood?
+    @show_neighbourhoods || service_areas.any?
+  end
+
+  def primary_neighbourhood?
+    # Show everything as primary if primary is not set
+    return true unless @site.primary_neighbourhood
+
+    @site.primary_neighbourhood && (@partner.address&.neighbourhood == @site.primary_neighbourhood)
+  end
+end
