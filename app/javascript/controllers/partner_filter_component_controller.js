d6a236190a9b3e3728c263b15b81ebc5dcc01e76
Dr Kim Foale <kim@gfsc.studio>
2024-08-16 15:22:06 +0100

    feat: Deal with nothing selected state, move some css around

--- app/javascript/controllers/partner_filter_component_controller.js
+++ app/javascript/controllers/partner_filter_component_controller.js
@@ -13,12 +13,14 @@ export default class extends Controller {
 	connect() {
 		// Find the associated label for each selected param and get the text contents
 		// If params are selected, they show up instead of "Category" and "Neighbourhood" text
-		this.categoryTextTarget.innerHTML = this.categoryTargets.find(
-			(r) => r.checked
-		).labels[0].textContent;
-		this.neighbourhoodTextTarget.innerHTML = this.neighbourhoodTargets.find(
-			(r) => r.checked
-		).labels[0].textContent;
+		if (this.selectedCategory) {
+			this.categoryTextTarget.innerHTML =
+				this.selectedCategory.labels[0].textContent;
+		}
+		if (this.selectedNeighbourhood) {
+			this.neighbourhoodTextTarget.innerHTML =
+				this.selectedNeighbourhood.labels[0].textContent;
+		}
 	}
 
 	submitCategory() {
@@ -30,12 +32,20 @@ export default class extends Controller {
 	}
 
 	resetCategory() {
-		this.categoryTargets.find((r) => r.checked).checked = false;
+		this.selectedCategory.checked = false;
 		this.formTarget.submit();
 	}
 
 	resetNeighbourhood() {
 		this.neighbourhoodTargets.find((r) => r.checked).checked = false;
 		this.formTarget.submit();
+	}
+
+	get selectedCategory() {
+		return this.categoryTargets.find((r) => r.checked);
+	}
+
+	get selectedNeighbourhood() {
+		return this.neighbourhoodTargets.find((r) => r.checked);
 	}
 }
