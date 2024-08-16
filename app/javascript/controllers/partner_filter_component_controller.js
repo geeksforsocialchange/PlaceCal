6331460cfcbdbb9f0ed13c56f70c13eee6b08327
Dr Kim Foale <kim@gfsc.studio>
2024-08-16 13:01:42 +0100

    feat: Radio buttons return IDs to console

--- app/javascript/controllers/partner_filter_component_controller.js
+++ app/javascript/controllers/partner_filter_component_controller.js
@@ -2,7 +2,17 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="partner-filter-component"
 export default class extends Controller {
+	static targets = ["category", "neighbourhood"];
+
 	connect() {
-		console.log("connected!!");
+		console.log(this.element);
+	}
+
+	submitCategory() {
+		console.log(this.categoryTargets.find((r) => r.checked).value);
+	}
+
+	submitNeighbourhood() {
+		console.log(this.neighbourhoodTargets.find((r) => r.checked).value);
 	}
 }
