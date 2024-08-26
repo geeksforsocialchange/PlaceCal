fe8a3b07bb049ebd8cfc9ce996aa90e7ad961b0d
Dr Kim Foale <kim@gfsc.studio>
2024-08-26 17:21:49 +0100

    feat: Add some basic tests

--- test/components/partner_preview_component_test.rb
+++ test/components/partner_preview_component_test.rb
@@ -3,10 +3,16 @@
 require 'test_helper'
 
 class PartnerPreviewComponentTest < ViewComponent::TestCase
+  setup do
+    @partner = create(:partner)
+    @site = create(:site)
+  end
+
   def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(PartnerPreviewComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+    render_inline(PartnerPreviewComponent.new(partner: @partner, site: @site))
+
+    assert_selector 'h3', text: @partner.name
+    assert_selector 'p', text: @partner.summary
+    assert_selector 'span', text: 'Hulme'
   end
 end
