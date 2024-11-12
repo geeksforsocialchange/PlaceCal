0628f6d169ff60485890fa3c0eb48efd1355f3fa
Dr Kim Foale <kim@gfsc.studio>
2024-11-12 17:25:06 +0000

    chore: Continue refactor

--- app/components/address_component.rb
+++ app/components/address_component.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 class AddressComponent < ViewComponent::Base
-  def initialize(address, raw_location: nil)
+  def initialize(address:, raw_location: nil)
     super
     @address = address
     @raw_location = raw_location
   end
 
   def formatted_address
-    if address.present?
-
-      address_lines = address.all_address_lines.map(&:strip)
-      return address_lines.join(', <br>').sanitize
+    if @address.present?
+      address_lines = @address.all_address_lines.map(&:strip)
+      return address_lines.join(', <br>').html_safe # rubocop:disable Rails/OutputSafety
     end
 
     uri = URI.parse(raw_location)
