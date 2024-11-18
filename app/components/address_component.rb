4a78e5f4c63339b53bf81c39b22a308b75151d84
Leni Kadali <52788034+lenikadali@users.noreply.github.com>
2024-11-18 19:05:57 +0300

    chore: Migrate the AddressComponent to using ViewComponent (#2638)
    Co-authored-by: Dr Kim Foale <kim@gfsc.studio>

---
+++ app/components/address_component.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class AddressComponent < ViewComponent::Base
+  def initialize(address:, raw_location: nil)
+    super
+    @address = address
+    @raw_location = raw_location
+  end
+
+  def formatted_address
+    if @address.present?
+      address_lines = @address.all_address_lines.map(&:strip)
+      return address_lines.join(", #{tag.br}")
+    end
+
+    uri = URI.parse(@raw_location)
+    "<a href='#{uri}'>#{uri.hostname}</a>"
+
+  rescue URI::InvalidURIError
+    @raw_location
+  end
+end
