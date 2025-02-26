40e960b3a5241a8a3be00cf3b9caeeabeacc7167
Leni Kadali <52788034+lenikadali@users.noreply.github.com>
2025-02-27 00:17:10 +0300

    Move template up a level, add all arguments
    Thanks to Rails auto-loading, it seems the AdminIndexComponent
    takes in more arguments than it appeared at first glance.
    As a result, in order to safely migrate the behaviour,
    we've added *all* the arguments that can be taken so that
    the controller tests pass.
    
    At a future date, we can remove the disabled cop and refactor
    the UI accordingly.
    
    Also moved the admin_index_component up a level so that
    it can be found as well as updating the component calls.

--- app/components/admin_index_component.rb
+++ app/components/admin_index_component.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 class AdminIndexComponent < ViewComponent::Base
-  def initialize(additional_links:, default: [])
+  # rubocop:disable Metrics/ParameterLists
+  def initialize(title:, model:, data:, new_link:, column_titles: [], columns: %i[], additional_links: [], default: [])
+    # rubocop:enable Metrics/ParameterLists
     super
+    @title = title
+    @model = model
+    @data = data
+    @new_link = new_link
+    @model = model
+    @column_titles = column_titles
+    @columns = columns
     @additional_links = additional_links
     @default = default
   end
 
   def model_name
-    properties[:model].to_s.chop.humanize
+    @model.to_s.chop.humanize
   end
 end
