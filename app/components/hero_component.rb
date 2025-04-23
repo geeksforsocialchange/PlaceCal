8bb2db15d276cae70014314899139eae0a74e640
Kerim Hudson <mail@kerimhudson.com>
2025-04-23 13:54:47 +0100

    chore: fixes based on comments

--- app/components/hero_component.rb
+++ app/components/hero_component.rb
@@ -3,8 +3,12 @@
 class HeroComponent < ViewComponent::Base
   def initialize(title, subtitle = '', schema = nil)
     super
-    @title = title.length > 32 ? title.split.in_groups(2, false).map { |g| g.join(' ') }.join('<br> ').html_safe : title # rubocop:disable Rails/OutputSafety
+    @title = clean_title(title)
     @subtitle = subtitle
     @schema = schema
+  end
+
+  def clean_title(title)
+    return title.length > 32 ? title.split.in_groups(2, false).map { |g| g.join(' ') }.join('<br> ').html_safe : title # rubocop:disable Rails/OutputSafety
   end
 end
