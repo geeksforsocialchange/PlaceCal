1fd303da12d65f8ffe05c59a68ac107cc0825f81
Kerim Hudson <mail@kerimhudson.com>
2025-04-23 15:15:45 +0100

    chore: fix boolean on html_safe

--- app/components/hero_component.rb
+++ app/components/hero_component.rb
@@ -10,7 +10,7 @@ class HeroComponent < ViewComponent::Base
 
   def clean_title(title)
     if title.length > 32
-      title.split.in_groups(2, false).map { |g| g.join(' ') }.join('<br> ').html_safe?
+      title.split.in_groups(2, false).map { |g| g.join(' ') }.join('<br> ').html_safe # rubocop:disable Rails/OutputSafety
     else
       title
     end
