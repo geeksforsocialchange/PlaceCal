1801d426c34fbae5bdd375e2a8f6a24fcc451aa3
Dr Kim Foale <kim@gfsc.studio>
2025-04-27 19:25:36 +0100

    chore: fix some migrations

--- db/migrate/20250424153646_change_sites_neighbourhoods_neighbourhood_id_null_constraint.rb
+++ db/migrate/20250424153646_change_sites_neighbourhoods_neighbourhood_id_null_constraint.rb
@@ -2,6 +2,13 @@
 
 class ChangeSitesNeighbourhoodsNeighbourhoodIdNullConstraint < ActiveRecord::Migration[7.2]
   def change
+    # Step 1: Clean up existing NULLs
+    # It's OK for this to not be reversible as they are bad entries we don't want
+    execute <<-SQL.squish # rubocop:disable Rails/ReversibleMigration
+        DELETE FROM sites_neighbourhoods WHERE neighbourhood_id IS NULL;
+    SQL
+
+    # Step 2: Now it's safe to add NOT NULL
     change_column_null :sites_neighbourhoods, :neighbourhood_id, false
   end
 end
