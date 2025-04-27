abe85d5088524d4b6ceea24ae2d7516dd0cba8c5
Dr Kim Foale <kim@gfsc.studio>
2025-04-27 19:25:36 +0100

    chore: fix some migrations

--- db/migrate/20250424153675_change_users_role_null_constraint.rb
+++ db/migrate/20250424153675_change_users_role_null_constraint.rb
@@ -2,6 +2,13 @@
 
 class ChangeUsersRoleNullConstraint < ActiveRecord::Migration[7.2]
   def change
+    # Step 1: Clean up existing NULLs
+    # It's OK for this to not be reversible as they are bad entries we don't want
+    execute <<-SQL.squish # rubocop:disable Rails/ReversibleMigration
+        DELETE FROM users WHERE role IS NULL;
+    SQL
+
+    # Step 2: Now it's safe to add NOT NULL
     change_column_null :users, :role, false
   end
 end
