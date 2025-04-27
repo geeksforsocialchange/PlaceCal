a61cf3685b89bef7acdebb94998092c869bdc6cb
Dr Kim Foale <kim@gfsc.studio>
2025-04-27 16:59:51 +0100

    chore: fix reversible migrations

--- db/migrate/20250424153639_remove_index_sites_tags_on_site_id_index.rb
+++ db/migrate/20250424153639_remove_index_sites_tags_on_site_id_index.rb
@@ -2,7 +2,9 @@
 
 class RemoveIndexSitesTagsOnSiteIdIndex < ActiveRecord::Migration[7.2]
   def change
-    # remove_index 'sites_tags', name: 'index_sites_tags_on_site_id'
-    remove_index :sites, :tags
+    reversible do |dir|
+      dir.up   { remove_index :sites_tags, name: 'index_sites_tags_on_site_id' }
+      dir.down { add_index :sites_tags, :site_id, name: 'index_sites_tags_on_site_id' }
+    end
   end
 end
