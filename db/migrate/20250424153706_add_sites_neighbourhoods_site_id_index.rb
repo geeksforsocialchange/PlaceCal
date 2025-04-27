d89da625b1e89fa8a8975e4eed0abf064e334fc0
Dr Kim Foale <kim@gfsc.studio>
2025-04-27 20:15:34 +0100

    chore: fix final migrations

--- db/migrate/20250424153706_add_sites_neighbourhoods_site_id_index.rb
+++ db/migrate/20250424153706_add_sites_neighbourhoods_site_id_index.rb
@@ -2,6 +2,6 @@
 
 class AddSitesNeighbourhoodsSiteIdIndex < ActiveRecord::Migration[7.2]
   def change
-    add_index :sites_neighbourhoods, :site_id, name: :index_sites_neighbourhoods_site_id, unique: true
+    add_index :sites_neighbourhoods, :site_id, name: :index_sites_neighbourhoods_site_id
   end
 end
