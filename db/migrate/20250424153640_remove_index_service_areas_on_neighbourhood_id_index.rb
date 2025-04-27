25953aa1c75481fe1937f58a6291ffb52c024c7b
Dr Kim Foale <kim@gfsc.studio>
2025-04-27 16:59:51 +0100

    chore: fix reversible migrations

--- db/migrate/20250424153640_remove_index_service_areas_on_neighbourhood_id_index.rb
+++ db/migrate/20250424153640_remove_index_service_areas_on_neighbourhood_id_index.rb
@@ -2,7 +2,9 @@
 
 class RemoveIndexServiceAreasOnNeighbourhoodIdIndex < ActiveRecord::Migration[7.2]
   def change
-    # remove_index 'service_areas', name: 'index_service_areas_on_neighbourhood_id'
-    remove_index :service_areas, :neighbourhoods
+    reversible do |dir|
+      dir.up   { remove_index 'service_areas', name: 'index_service_areas_on_neighbourhood_id' }
+      dir.down { add_index 'service_areas', :site_id, name: 'index_service_areas_on_neighbourhood_id' }
+    end
   end
 end
--- db/migrate/20250424153642_remove_index_article_partners_on_article_id_index.rb
+++ db/migrate/20250424153642_remove_index_article_partners_on_article_id_index.rb
@@ -2,7 +2,9 @@
 
 class RemoveIndexArticlePartnersOnArticleIdIndex < ActiveRecord::Migration[7.2]
   def change
-    # remove_index 'article_partners', name: 'index_article_partners_on_article_id'
-    remove_index :article, :partners
+    reversible do |dir|
+      dir.up   { remove_index 'article_partners', name: 'index_article_partners_on_article_id' }
+      dir.down { add_index 'article_partners', :site_id, name: 'index_article_partners_on_article_id' }
+    end
   end
 end
