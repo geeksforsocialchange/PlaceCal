71982ef54312142eedefea6b55c171cc8d5d25fc
Rix <makepeacerp@gmail.com>
2025-05-25 10:50:19 +0100

    Replace the breadcrumb component

--- app/components/breadcrumb/breadcrumb_component.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# app/components/breadcrumb_component.rb
-class BreadcrumbComponent < MountainView::Presenter
-  property :trail, default: []
-  property :site_name, default: 'The Community Calendar'
-end
