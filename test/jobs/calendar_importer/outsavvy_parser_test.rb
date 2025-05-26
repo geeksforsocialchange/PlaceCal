3e5bec9b74df7e607fc12b3731c47d1d2023dde1
James Walker <james.walker@bath.edu>
2025-05-26 11:54:19 +0100

    Tests for OutSavvy parser

--- test/jobs/calendar_importer/outsavvy_parser_test.rb
+++ test/jobs/calendar_importer/outsavvy_parser_test.rb
@@ -3,8 +3,9 @@
 require 'test_helper'
 
 class OutsavvyParserTest < ActiveSupport::TestCase
-  test 'extracts event URLs from organiser page' do
+  test 'extracts event URLs from organiser page with live events' do
     outsavvy_url = 'https://www.outsavvy.com/organiser/a-whole-orange'
+    url_pattern = %r{^https://www\.outsavvy\.com/event/[A-Za-z0-9-]+}
 
     VCR.use_cassette(:outsavvy_events) do
       calendar = build(
@@ -17,7 +18,29 @@ class OutsavvyParserTest < ActiveSupport::TestCase
       parser = CalendarImporter::Parsers::Outsavvy.new(calendar, url: outsavvy_url)
 
       data = parser.extract_event_urls
-      assert_equal 6, data.length
+
+      data.map do |url|
+        assert_match(url_pattern, url)
+      end
+    end
+  end
+
+  test 'returns empty list for organiser page with no events' do
+    outsavvy_url = 'https://www.outsavvy.com/organiser/treacles'
+
+    VCR.use_cassette(:outsavvy_no_events) do
+      calendar = build(
+        :calendar,
+        strategy: :event,
+        name: :import_test_calendar,
+        source: outsavvy_url
+      )
+
+      parser = CalendarImporter::Parsers::Outsavvy.new(calendar, url: outsavvy_url)
+
+      data = parser.extract_event_urls
+
+      assert_empty(data)
     end
   end
 end
