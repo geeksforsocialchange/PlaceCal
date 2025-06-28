a76d6b3f7f97b796504015e82602dc07c68ec2ae
Rix <makepeacerp@gmail.com>
2025-06-28 09:39:02 +0100

    Don't assert on meeting number

--- test/components/event_component_test.rb
+++ test/components/event_component_test.rb
@@ -18,7 +18,7 @@ class EventComponentTest < ViewComponent::TestCase
     with_request_url('/events/11234.json', host: 'mossley.mossley.localhost') do
       test = render_inline(EventComponent.new(site: @site, context: @context, event: @event, primary_neighbourhood: @primary_neighbourhood, show_neighbourhoods: @show_neighbourhoods))
       puts test
-      assert_text 'N.A. (Narcotics Anonymous) - Meetup 1'
+      assert_text 'N.A. (Narcotics Anonymous) - Meetup '
       assert_text '123 Moss Ln E'
       assert_text '12am –  2am'
       assert_text '2 hours'
