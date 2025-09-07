8fb29216a2df8dcc535e36ff9ecbae4ea5a1a29c
WheresAlice <wheresalice@noreply.users.github.com>
2025-09-07 10:38:14 +0100

    fix(models): remove https-only URL validation and add example site
    Remove strict HTTPS URL validation to allow HTTP URLs during development
    Add an example site that is published to make developer onboarding nicer

--- db/seeds/004_sites.rb
+++ db/seeds/004_sites.rb
@@ -9,6 +9,22 @@ module SeedTags
       slug: 'default-site',
       url: 'http://default-site.lvh.me:3000'
     )
+    area = Neighbourhood.create!(
+      name: 'Ad Astra',
+      name_abbr: 'Ad Astra',
+      unit: 'country',
+      unit_code_key: 'CTRY99AA',
+      unit_code_value: 'E90210aaa',
+      unit_name: 'Ad Astra',
+      release_date: DateTime.now
+    )
+    Site.create!(
+      name: 'Ad Astra',
+      slug: 'ad-astra',
+      url: 'http://ad-astra.lvh.me:3000',
+      is_published: true,
+      primary_neighbourhood: area
+    )
   end
 end
 
