2510006c0f55a708587f260b93bf3de01dd31d96
Quinn Daley <quinn@fishpercolator.co.uk>
2025-11-05 13:54:51 +0000

    Add some very basic unit tests

--- test/models/site_test.rb
+++ test/models/site_test.rb
@@ -17,6 +17,43 @@ class SiteTest < ActiveSupport::TestCase
   end
 end
 
+class CustomDomainTest < ActiveSupport::TestCase
+  # We could use webmock here but we're not actually simulating the HTTP part so we'll make a little mock struct
+  MockRequest = Struct.new(:host) do
+    def subdomains
+      host.split('.')
+    end
+
+    def subdomain
+      subdomains.first
+    end
+  end
+
+  setup do
+    @site = create(:site, url: 'https://wibble.example.com/', slug: 'wibblecal')
+  end
+
+  test 'site is returned for a request that has domain' do
+    request = MockRequest.new('wibble.example.com')
+    assert_equal Site.find_by_request(request), @site
+  end
+
+  test 'site is returned for slug' do
+    request = MockRequest.new('wibblecal.placecal.org')
+    assert_equal Site.find_by_request(request), @site
+  end
+
+  test 'site is returned for www.slug' do
+    request = MockRequest.new('www.wibblecal.placecal.org')
+    assert_equal Site.find_by_request(request), @site
+  end
+
+  test 'site is not returned for a request that has neither' do
+    request = MockRequest.new('nothing.placecal.org')
+    assert_not_equal Site.find_by_request(request), @site
+  end
+end
+
 class SitePartnerTest < ActiveSupport::TestCase
   test 'can find sites by partner' do
     address_neighbourhood = FactoryBot.create(:neighbourhood)
