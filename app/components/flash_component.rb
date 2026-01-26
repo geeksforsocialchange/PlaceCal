a30ee9d15e73e6cd1f855f5675a7186149e06a21
Dr Kim Foale <kim@gfsc.studio>
2026-01-26 13:01:24 +0000

    fix: Use simple FlashComponent for public site
    The public layout was using Admin::FlashComponent which renders SVG
    icons with Tailwind classes (w-6 h-6). These classes don't exist on
    the public site CSS, causing the icon to render at full size.
    
    Created a simple FlashComponent that uses the existing SCSS alert
    styles instead of Tailwind/daisyUI components.

---
+++ app/components/flash_component.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class FlashComponent < ViewComponent::Base
+  def initialize(flash: nil)
+    super
+    @flash = flash
+  end
+
+  def flash
+    @flash || helpers.flash
+  end
+
+  def render?
+    flash.any?
+  end
+
+  def alert_class(key)
+    case key.to_sym
+    when :success, :notice then 'alert-success'
+    when :danger, :alert, :error then 'alert-danger'
+    else 'alert-success'
+    end
+  end
+end
