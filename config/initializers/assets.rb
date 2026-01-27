a75822208eb70c46aa94bdbda548f2444170944a
Dr Kim Foale <kim@gfsc.studio>
2026-01-27 14:20:58 +0000

    Move es-module-shims.js to vendor/assets/javascripts
    Removes confusing app/javascripts folder (vs app/javascript).
    vendor/assets/javascripts is the standard Rails location for third-party JS.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,6 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-Rails.application.config.assets.paths << Rails.root.join('app', 'javascripts')
 Rails.application.config.assets.paths << Rails.root.join('app', 'javascript')
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'pdfs')
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
