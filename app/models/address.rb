d034d4ec4d9528e9634e9446c5f5557e70bfa9bd
Dr Kim Foale <kim@gfsc.studio>
2026-03-30 16:17:33 +0100

    Fix duplicate build_from_components and misplaced private in Address
    Cherry-pick conflict resolution left a duplicate method definition and
    an extra private keyword. Consolidate to single definition with class
    methods before private, instance methods after.

--- app/models/address.rb
+++ app/models/address.rb
@@ -71,6 +71,18 @@ class Address < ApplicationRecord
     address.save ? address : nil
   end
 
+  # Delete addresses not referenced by any partner or event.
+  # Returns the number of deleted rows.
+  def self.delete_orphaned!
+    in_use_ids = Set.new(Partner.pluck(:address_id).compact) |
+                 Set.new(Event.pluck(:address_id).compact)
+
+    orphaned = where.not(id: in_use_ids)
+    count = orphaned.count
+    orphaned.in_batches(of: 1000).delete_all if count.positive?
+    count
+  end
+
   private
 
   # Set the (lat,lon) and neighbourhood from address data.
@@ -104,32 +116,6 @@ class Address < ApplicationRecord
     self.longitude = res['longitude']
     self.latitude = res['latitude']
   end
-
-  def self.build_from_components(components, postcode)
-    return if components.blank?
-
-    address = Address.new(
-      street_address: components[0]&.strip,
-      street_address2: components[1]&.strip,
-      street_address3: components[2]&.strip,
-      postcode: postcode
-    )
-    address.save ? address : nil
-  end
-
-  # Delete addresses not referenced by any partner or event.
-  # Returns the number of deleted rows.
-  def self.delete_orphaned!
-    in_use_ids = Set.new(Partner.pluck(:address_id).compact) |
-                 Set.new(Event.pluck(:address_id).compact)
-
-    orphaned = where.not(id: in_use_ids)
-    count = orphaned.count
-    orphaned.in_batches(of: 1000).delete_all if count.positive?
-    count
-  end
-
-  private
 
   def refresh_neighbourhood_partners_count
     # Refresh current neighbourhood
