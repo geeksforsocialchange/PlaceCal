c9854455a576550e6ef5e384d76061150deb4e6e
jo <git@akki.uk>
2026-04-08 15:37:15 +0100

    reformat Navigation tailwind classes, normalise distances, make :site prop not nilable

--- spec/components/navigation_component_spec.rb
+++ spec/components/navigation_component_spec.rb
@@ -3,31 +3,26 @@
 require "rails_helper"
 
 RSpec.describe Components::Navigation, type: :component do
-  let(:navigation) { [["Events", "/events"], ["Partners", "/partners"]] }
+  let(:navigation) { [["Home", "/"], ["Events", "/events"], ["Partners", "/partners"]] }
+  let(:default_site) { create(:default_site) }
 
   it "renders navigation links" do
-    render_inline(described_class.new(navigation: navigation))
+    render_inline(described_class.new(navigation: navigation, site: default_site))
 
     expect(page).to have_link("Events", href: "/events")
     expect(page).to have_link("Partners", href: "/partners")
   end
 
   it "renders home link" do
-    render_inline(described_class.new(navigation: navigation))
+    render_inline(described_class.new(navigation: navigation, site: default_site))
 
     expect(page).to have_link("Home")
   end
 
   it "renders header structure" do
-    render_inline(described_class.new(navigation: navigation))
+    render_inline(described_class.new(navigation: navigation, site: default_site))
 
     expect(page).to have_css(".header")
     expect(page).to have_css("nav.nav")
-  end
-
-  it "handles empty navigation" do
-    render_inline(described_class.new(navigation: []))
-
-    expect(page).to have_link("Home")
   end
 end
