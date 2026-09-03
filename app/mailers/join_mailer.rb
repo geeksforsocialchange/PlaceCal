fab3f96af5a1f578dd54ef436f69f22da4d6f649
Dr Kim Foale <kim@gfsc.studio>
2026-09-03 20:55:32 +0100

    WP 3.6: strip newlines from the site name in the join mail subject
    A CR or LF in an interpolated site name would let it inject extra mail
    headers.

--- app/mailers/join_mailer.rb
+++ app/mailers/join_mailer.rb
@@ -3,8 +3,10 @@
 class JoinMailer < ApplicationMailer
   def join_us(join)
     site = join.site
+    # A newline in the interpolated name would let it inject extra mail
+    # headers, so it never reaches the Subject header intact.
     subject = if site
-                t('join_mailer.join_us.subject_with_site', site: site.name)
+                t('join_mailer.join_us.subject_with_site', site: site.name.to_s.delete("\r\n"))
               else
                 t('join_mailer.join_us.subject')
               end
